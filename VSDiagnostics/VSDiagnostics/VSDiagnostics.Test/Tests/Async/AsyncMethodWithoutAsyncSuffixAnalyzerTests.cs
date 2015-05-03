dc5fb9480030c4ab89e18e6d12ab8cff26484e59
Vannevelj <jer_vannevel@hotmail.com>
2015-05-03 22:53:41 +0200

    Added AsyncMethodWithoutAsyncSuffixAnalyzer #14

---
+++ VSDiagnostics/VSDiagnostics/VSDiagnostics.Test/Tests/Async/AsyncMethodWithoutAsyncSuffixAnalyzerTests.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestHelper;
+using VSDiagnostics.Diagnostics.Async;
+using VSDiagnostics.Diagnostics.Exceptions.EmptyArgumentExceptionAnalyzer;
+
+namespace VSDiagnostics.Test.Tests.Async
+{
+    [TestClass]
+    public class AsyncMethodWithoutAsyncSuffixAnalyzerTests : CodeFixVerifier
+    {
+        [TestMethod]
+        public void AsyncMethodWithoutAsyncSuffixAnalyzer_WithAsyncKeywordAndNoSuffix_InvokesWarning()
+        {
+            var test = @"
+    using System;
+    using System.Text;
+
+    namespace ConsoleApplication1
+    {
+        class MyClass
+        {   
+            async Task Method()
+            {
+                
+            }
+        }
+    }";
+
+            var expectedDiagnostic = new DiagnosticResult
+            {
+                Id = AsyncMethodWithoutAsyncSuffixAnalyzer.DiagnosticId,
+                Message = string.Format(AsyncMethodWithoutAsyncSuffixAnalyzer.Message, "Method"),
+                Severity = EmptyArgumentExceptionAnalyzer.Severity,
+                Locations =
+                    new[]
+                    {
+                        new DiagnosticResultLocation("Test0.cs", 9, 13)
+                    }
+            };
+
+            VerifyCSharpDiagnostic(test, expectedDiagnostic);
+        }
+
+        [TestMethod]
+        public void AsyncMethodWithoutAsyncSuffixAnalyzer_WithAsyncKeywordAndSuffix_DoesNotDisplayWarning()
+        {
+            var test = @"
+    using System;
+    using System.Text;
+
+    namespace ConsoleApplication1
+    {
+        class MyClass
+        {   
+            async Task MethodAsync()
+            {
+                
+            }
+        }
+    }";
+            VerifyCSharpDiagnostic(test);
+        }
+
+        [TestMethod]
+        public void AsyncMethodWithoutAsyncSuffixAnalyzer_WithoutAsyncKeywordAndSuffix_DoesNotDisplayWarning()
+        {
+            var test = @"
+    using System;
+    using System.Text;
+
+    namespace ConsoleApplication1
+    {
+        class MyClass
+        {   
+            void Method()
+            {
+                
+            }
+        }
+    }";
+            VerifyCSharpDiagnostic(test);
+        }
+
+        protected override DiagnosticAnalyzer GetCSharpDiagnosticAnalyzer()
+        {
+            return new AsyncMethodWithoutAsyncSuffixAnalyzer();
+        }
+    }
+}
---
+++ VSDiagnostics/VSDiagnostics/VSDiagnostics/Diagnostics/Async/AsyncMethodWithoutAsyncSuffixAnalyzer.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Immutable;
+using System.Linq;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace VSDiagnostics.Diagnostics.Async
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class AsyncMethodWithoutAsyncSuffixAnalyzer : DiagnosticAnalyzer
+    {
+        public const string DiagnosticId = nameof(AsyncMethodWithoutAsyncSuffixAnalyzer);
+        internal const string Title = "Verifies whether an async method has the 'Async' suffix.";
+        internal const string Message = "Method \"{0}\" does not end with 'Async'.";
+        internal const string Category = "Async";
+        internal const DiagnosticSeverity Severity = DiagnosticSeverity.Warning;
+        internal static DiagnosticDescriptor Rule = new DiagnosticDescriptor(DiagnosticId, Title, Message, Category, Severity, true);
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(Rule);
+
+        public override void Initialize(AnalysisContext context)
+        {
+            context.RegisterSyntaxNodeAction(AnalyzeSyntaxNode, SyntaxKind.MethodDeclaration);
+        }
+
+        private static void AnalyzeSyntaxNode(SyntaxNodeAnalysisContext context)
+        {
+            var method = context.Node as MethodDeclarationSyntax;
+            if (method == null)
+            {
+                return;
+            }
+
+            if (method.Modifiers.Any(x => x.IsKind(SyntaxKind.AsyncKeyword)))
+            {
+                if (!method.Identifier.Text.EndsWith("Async"))
+                {
+                    context.ReportDiagnostic(Diagnostic.Create(Rule, method.GetLocation(), method.Identifier.Text));
+                }
+            }
+        }
+    }
+}
