bccd8218b83f3c2352472efda378e731a9eb6ecc
Vannevelj <jer_vannevel@hotmail.com>
2015-05-03 23:40:14 +0200

    Added the TestMethodWithoutPublicModifier analyzer + tests #11

---
+++ VSDiagnostics/VSDiagnostics/VSDiagnostics.Test/Tests/Tests/TestMethodWithoutPublicModifierAnalyzerTests.cs
@@ -0,0 +1,212 @@
+﻿using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestHelper;
+using VSDiagnostics.Diagnostics.Tests.TestMethodWithoutPublicModifierAnalyzer;
+
+namespace VSDiagnostics.Test.Tests.Tests
+{
+    [TestClass]
+    public class TestMethodWithoutPublicModifierAnalyzerTests : CodeFixVerifier
+    {
+        [TestMethod]
+        public void TestMethodWithoutPublicModifierAnalyzer_WithPublicModifierAndTestAttribute_DoesNotInvokeWarning()
+        {
+            var test = @"
+    using System;
+    using System.Text;
+
+    namespace ConsoleApplication1
+    {
+        [TestFixture]
+        public class MyClass
+        {   
+            [Test]
+            public void Method()
+            {
+                
+            }
+        }
+    }";
+
+            VerifyCSharpDiagnostic(test);
+        }
+
+        [TestMethod]
+        public void TestMethodWithoutPublicModifierAnalyzer_WithPublicModifierAndTestMethodAttribute_DoesNotInvokeWarning()
+        {
+            var test = @"
+    using System;
+    using System.Text;
+
+    namespace ConsoleApplication1
+    {
+        [TestClass]
+        public class MyClass
+        {   
+            [TestMethod]
+            public void Method()
+            {
+                
+            }
+        }
+    }";
+
+            VerifyCSharpDiagnostic(test);
+        }
+
+        [TestMethod]
+        public void TestMethodWithoutPublicModifierAnalyzer_WithPublicModifierAndFactAttribute_DoesNotInvokeWarning()
+        {
+            var test = @"
+    using System;
+    using System.Text;
+
+    namespace ConsoleApplication1
+    {
+        public class MyClass
+        {   
+            [Fact]
+            public void Method()
+            {
+                
+            }
+        }
+    }";
+
+            VerifyCSharpDiagnostic(test);
+        }
+
+        [TestMethod]
+        public void TestMethodWithoutPublicModifierAnalyzer_WithInternalModifierAndTestAttribute_InvokesWarning()
+        {
+            var test = @"
+    using System;
+    using System.Text;
+
+    namespace ConsoleApplication1
+    {
+        [TestFixture]
+        public class MyClass
+        {   
+            [Test]
+            internal void Method()
+            {
+                
+            }
+        }
+    }";
+
+            var expectedDiagnostic = new DiagnosticResult
+            {
+                Id = TestMethodWithoutPublicModifierAnalyzer.DiagnosticId,
+                Message = string.Format(TestMethodWithoutPublicModifierAnalyzer.Message, "Method"),
+                Severity = TestMethodWithoutPublicModifierAnalyzer.Severity,
+                Locations =
+                    new[]
+                    {
+                        new DiagnosticResultLocation("Test0.cs", 10, 13)
+                    }
+            };
+
+            VerifyCSharpDiagnostic(test, expectedDiagnostic);
+        }
+
+        [TestMethod]
+        public void TestMethodWithoutPublicModifierAnalyzer_WithInternalModifierAndTestMethodAttribute_InvokesWarning()
+        {
+            var test = @"
+    using System;
+    using System.Text;
+
+    namespace ConsoleApplication1
+    {
+        [TestClass]
+        public class MyClass
+        {   
+            [TestMethod]
+            internal void Method()
+            {
+                
+            }
+        }
+    }";
+
+            var expectedDiagnostic = new DiagnosticResult
+            {
+                Id = TestMethodWithoutPublicModifierAnalyzer.DiagnosticId,
+                Message = string.Format(TestMethodWithoutPublicModifierAnalyzer.Message, "Method"),
+                Severity = TestMethodWithoutPublicModifierAnalyzer.Severity,
+                Locations =
+                    new[]
+                    {
+                        new DiagnosticResultLocation("Test0.cs", 10, 13)
+                    }
+            };
+
+            VerifyCSharpDiagnostic(test, expectedDiagnostic);
+        }
+
+        [TestMethod]
+        public void TestMethodWithoutPublicModifierAnalyzer_WithInternalModifierAndFactAttribute_InvokesWarning()
+        {
+            var test = @"
+    using System;
+    using System.Text;
+
+    namespace ConsoleApplication1
+    {
+        public class MyClass
+        {   
+            [Fact]
+            internal void Method()
+            {
+                
+            }
+        }
+    }";
+
+            var expectedDiagnostic = new DiagnosticResult
+            {
+                Id = TestMethodWithoutPublicModifierAnalyzer.DiagnosticId,
+                Message = string.Format(TestMethodWithoutPublicModifierAnalyzer.Message, "Method"),
+                Severity = TestMethodWithoutPublicModifierAnalyzer.Severity,
+                Locations =
+                    new[]
+                    {
+                        new DiagnosticResultLocation("Test0.cs", 9, 13)
+                    }
+            };
+
+            VerifyCSharpDiagnostic(test, expectedDiagnostic);
+        }
+
+        [TestMethod]
+        public void TestMethodWithoutPublicModifierAnalyzer_WithPublicModifierAndMultipleAttributes_DoesNotInvokeWarning()
+        {
+            var test = @"
+    using System;
+    using System.Text;
+
+    namespace ConsoleApplication1
+    {
+        [TestFixture]
+        public class MyClass
+        {    
+            [Ignore]
+            [Test]
+            public void Method()
+            {
+                
+            }
+        }
+    }";
+
+            VerifyCSharpDiagnostic(test);
+        }
+
+        protected override DiagnosticAnalyzer GetCSharpDiagnosticAnalyzer()
+        {
+            return new TestMethodWithoutPublicModifierAnalyzer();
+        }
+    }
+}
---
+++ VSDiagnostics/VSDiagnostics/VSDiagnostics/Diagnostics/Tests/TestMethodWithoutPublicModifierAnalyzer/TestMethodWithoutPublicModifierAnalyzer.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Immutable;
+using System.Linq;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace VSDiagnostics.Diagnostics.Tests.TestMethodWithoutPublicModifierAnalyzer
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class TestMethodWithoutPublicModifierAnalyzer : DiagnosticAnalyzer
+    {
+        public const string DiagnosticId = nameof(TestMethodWithoutPublicModifierAnalyzer);
+        internal const string Title = "Verifies whether a test method has the public modifier.";
+        internal const string Message = "Test method \"{0}\" is not public.";
+        internal const string Category = "Tests";
+        internal const DiagnosticSeverity Severity = DiagnosticSeverity.Warning;
+        internal static DiagnosticDescriptor Rule = new DiagnosticDescriptor(DiagnosticId, Title, Message, Category, Severity, true);
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(Rule);
+
+        public override void Initialize(AnalysisContext context)
+        {
+            context.RegisterSyntaxNodeAction(AnalyzeNode, SyntaxKind.MethodDeclaration);
+        }
+
+        private void AnalyzeNode(SyntaxNodeAnalysisContext context)
+        {
+            var method = context.Node as MethodDeclarationSyntax;
+            if (method == null)
+            {
+                return;
+            }
+
+            if (IsTestMethod(method))
+            {
+                if (!method.Modifiers.Any(SyntaxKind.PublicKeyword))
+                {
+                    context.ReportDiagnostic(Diagnostic.Create(Rule, method.GetLocation(), method.Identifier.Text));
+                }
+            }
+        }
+
+        private static bool IsTestMethod(MethodDeclarationSyntax method)
+        {
+            var methodAttributes = new[] { "Test", "TestMethod", "Fact" };
+
+            return method.AttributeLists.First().Attributes.Any(x => methodAttributes.Contains(x.Name.ToString()));
+        }
+    }
+}
