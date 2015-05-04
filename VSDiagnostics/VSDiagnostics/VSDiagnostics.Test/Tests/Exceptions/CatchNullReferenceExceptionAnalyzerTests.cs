78fd77b986d902f08276a1e45b7f369d63f9ebe4
Vannevelj <jer_vannevel@hotmail.com>
2015-05-04 23:07:32 +0200

    Implemented CatchNullReferenceExceptionAnalyzer #18

---
+++ VSDiagnostics/VSDiagnostics/VSDiagnostics.Test/Tests/Exceptions/CatchNullReferenceExceptionAnalyzerTests.cs
@@ -0,0 +1,108 @@
+﻿using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestHelper;
+using VSDiagnostics.Diagnostics.Exceptions.CatchNullReferenceException;
+
+namespace VSDiagnostics.Test.Tests.Exceptions
+{
+    [TestClass]
+    public class CatchNullReferenceExceptionAnalyzerTests : DiagnosticVerifier
+    {
+        [TestMethod]
+        public void CatchNullReferenceExceptionAnalyzer_WithNullReferenceCatchClause_InvokesWarning()
+        {
+            var test = @"
+    using System;
+    using System.Text;
+
+    namespace ConsoleApplication1
+    {
+        class MyClass
+        {   
+            void Method(string input)
+            {
+                try 
+                {
+                }
+                catch(NullReferenceException e)
+                {
+
+                }
+            }
+        }
+    }";
+
+            var expectedDiagnostic = new DiagnosticResult
+            {
+                Id = CatchNullReferenceExceptionAnalyzer.DiagnosticId,
+                Message = CatchNullReferenceExceptionAnalyzer.Message,
+                Severity = CatchNullReferenceExceptionAnalyzer.Severity,
+                Locations = new[]
+                {
+                    new DiagnosticResultLocation("Test0.cs", 14, 22)
+                }
+            };
+
+            VerifyCSharpDiagnostic(test, expectedDiagnostic);
+        }
+
+        [TestMethod]
+        public void CatchNullReferenceExceptionAnalyzer_WithoutNullReferenceCatchClause_DoesNotInvokeWarning()
+        {
+            var test = @"
+    using System;
+    using System.Text;
+
+    namespace ConsoleApplication1
+    {
+        class MyClass
+        {   
+            void Method(string input)
+            {
+                try 
+                {
+                }
+                catch(ArgumentException e)
+                {
+
+                }
+            }
+        }
+    }";
+
+            VerifyCSharpDiagnostic(test);
+        }
+
+        [TestMethod]
+        public void CatchNullReferenceExceptionAnalyzer_WithEmptyCatchClause_DoesNotInvokeWarning()
+        {
+            var test = @"
+    using System;
+    using System.Text;
+
+    namespace ConsoleApplication1
+    {
+        class MyClass
+        {   
+            void Method(string input)
+            {
+                try 
+                {
+                }
+                catch
+                {
+
+                }
+            }
+        }
+    }";
+
+            VerifyCSharpDiagnostic(test);
+        }
+
+        protected override DiagnosticAnalyzer GetCSharpDiagnosticAnalyzer()
+        {
+            return new CatchNullReferenceExceptionAnalyzer();
+        }
+    }
+}
---
+++ VSDiagnostics/VSDiagnostics/VSDiagnostics/Diagnostics/Exceptions/CatchNullReferenceException/CatchNullReferenceExceptionAnalyzer.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Immutable;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace VSDiagnostics.Diagnostics.Exceptions.CatchNullReferenceException
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class CatchNullReferenceExceptionAnalyzer : DiagnosticAnalyzer
+    {
+        public const string DiagnosticId = nameof(CatchNullReferenceExceptionAnalyzer);
+        internal const string Title = "Verifies whether no NullReferenceExceptions are caught.";
+        internal const string Message = "A catch clause catches NullReferenceException. Consider using != null or null propagation instead.";
+        internal const string Category = "Exceptions";
+        internal const DiagnosticSeverity Severity = DiagnosticSeverity.Warning;
+        internal static DiagnosticDescriptor Rule = new DiagnosticDescriptor(DiagnosticId, Title, Message, Category, Severity, true);
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(Rule);
+
+        public override void Initialize(AnalysisContext context)
+        {
+            context.RegisterSyntaxNodeAction(AnalyzeSyntaxNode, SyntaxKind.CatchDeclaration);
+        }
+
+        private void AnalyzeSyntaxNode(SyntaxNodeAnalysisContext context)
+        {
+            var catchDeclaration = context.Node as CatchDeclarationSyntax;
+            if (catchDeclaration == null)
+            {
+                return;
+            }
+
+            var catchType = catchDeclaration.Type;
+            if (catchType == null)
+            {
+                return;
+            }
+
+            var catchSymbol = context.SemanticModel.GetSymbolInfo(catchType).Symbol.MetadataName;
+            if (catchSymbol == "NullReferenceException")
+            {
+                context.ReportDiagnostic(Diagnostic.Create(Rule, catchDeclaration.GetLocation()));
+            }
+        }
+    }
+}
