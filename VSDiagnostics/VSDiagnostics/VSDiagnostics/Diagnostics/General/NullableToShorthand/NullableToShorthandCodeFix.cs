28caec6f6888bc08e22ca9b1906b2ff01f7e4ef5
Vannevelj <jer_vannevel@hotmail.com>
2015-05-08 21:17:52 +0200

    Eureka! Shit's sort of working! #12

--- VSDiagnostics/VSDiagnostics/VSDiagnostics/Diagnostics/General/NullableToShorthand/NullableToShorthandCodeFix.cs
+++ VSDiagnostics/VSDiagnostics/VSDiagnostics/Diagnostics/General/NullableToShorthand/NullableToShorthandCodeFix.cs
@@ -1,9 +1,13 @@
-﻿using System;
-using System.Collections.Immutable;
+﻿using System.Collections.Immutable;
 using System.Composition;
+using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeActions;
 using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Formatting;
 
 namespace VSDiagnostics.Diagnostics.General.NullableToShorthand
 {
@@ -20,9 +24,31 @@ namespace VSDiagnostics.Diagnostics.General.NullableToShorthand
             return WellKnownFixAllProviders.BatchFixer;
         }
 
-        public override Task ComputeFixesAsync(CodeFixContext context)
+        public override async Task ComputeFixesAsync(CodeFixContext context)
         {
-            throw new NotImplementedException();
+            var root = await context.Document.GetSyntaxRootAsync(context.CancellationToken).ConfigureAwait(false);
+            var diagnostic = context.Diagnostics.First();
+            var diagnosticSpan = diagnostic.Location.SourceSpan;
+
+            var declaration = root.FindToken(diagnosticSpan.Start);
+            context.RegisterFix(CodeAction.Create("Use shorthand notation", x => UseShorthandNotation(context.Document, root, declaration)), diagnostic);
+        }
+
+        private static async Task<Solution> UseShorthandNotation(Document document, SyntaxNode root, SyntaxToken declaration)
+        {
+            var node = root.FindNode(declaration.Span);
+            var typeNode = (GenericNameSyntax) node;
+            var semanticModel = await document.GetSemanticModelAsync();
+            var type = semanticModel.GetTypeInfo(typeNode.TypeArgumentList.Arguments.First()).Type;
+
+            var newExpression = SyntaxFactory.ParseTypeName(type.MetadataName + "?").WithLeadingTrivia(node.GetLeadingTrivia()).WithTrailingTrivia(SyntaxFactory.Space);
+
+            var newParent = node.ReplaceNode(node, newExpression);
+            var newRoot = root.ReplaceNode(node, newParent);
+            var formattedRoot = Formatter.Format(newRoot, newParent.Span, document.Project.Solution.Workspace, document.Project.Solution.Workspace.Options);
+            var newDocument = document.WithSyntaxRoot(formattedRoot);
+
+            return newDocument.Project.Solution;
         }
     }
 }
