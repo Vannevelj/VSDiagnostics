08e0cc868077cbbc0b2469eee81dfe8859b63e0c
Vannevelj <jer_vannevel@hotmail.com>
2015-05-26 00:39:39 +0200

    Implemented the tests and general structure of the ConditionalOperatorReturnsDefaultOptions diagnostic

---
+++ VSDiagnostics/VSDiagnostics/VSDiagnostics/Diagnostics/General/ConditionalOperatorReturnsDefaultOptions/ConditionalOperatorReturnsDefaultOptionsAnalyzer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Immutable;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace VSDiagnostics.Diagnostics.General.ConditionalOperatorReturnsDefaultOptions
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class ConditionalOperatorReturnsDefaultOptionsAnalyzer : DiagnosticAnalyzer
+    {
+        public const string DiagnosticId = nameof(ConditionalOperatorReturnsDefaultOptionsAnalyzer);
+        internal const string Title = "The conditional operator shouldn't return just literals.";
+        internal const string Message = "A conditional operator can be omitted.";
+        internal const string Category = "General";
+        internal const DiagnosticSeverity Severity = DiagnosticSeverity.Warning;
+        internal static DiagnosticDescriptor Rule = new DiagnosticDescriptor(DiagnosticId, Title, Message, Category, Severity, true);
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(Rule);
+
+        public override void Initialize(AnalysisContext context)
+        {
+            context.RegisterSyntaxNodeAction(AnalyzeSymbol, SyntaxKind.ConditionalExpression);
+        }
+
+        private void AnalyzeSymbol(SyntaxNodeAnalysisContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ VSDiagnostics/VSDiagnostics/VSDiagnostics/Diagnostics/General/ConditionalOperatorReturnsDefaultOptions/ConditionalOperatorReturnsDefaultOptionsCodeFix.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Immutable;
+using System.Composition;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeActions;
+using Microsoft.CodeAnalysis.CodeFixes;
+
+namespace VSDiagnostics.Diagnostics.General.ConditionalOperatorReturnsDefaultOptions
+{
+    [ExportCodeFixProvider("ConditionalOperatorReturnsDefaultOptions", LanguageNames.CSharp), Shared]
+    public class ConditionalOperatorReturnsDefaultOptionsCodeFix : CodeFixProvider
+    {
+        public override ImmutableArray<string> FixableDiagnosticIds => ImmutableArray.Create(ConditionalOperatorReturnsDefaultOptionsAnalyzer.DiagnosticId);
+
+        public override FixAllProvider GetFixAllProvider()
+        {
+            return WellKnownFixAllProviders.BatchFixer;
+        }
+
+        public override async Task RegisterCodeFixesAsync(CodeFixContext context)
+        {
+            var root = await context.Document.GetSyntaxRootAsync(context.CancellationToken).ConfigureAwait(false);
+            var diagnostic = context.Diagnostics.First();
+            var diagnosticSpan = diagnostic.Location.SourceSpan;
+
+            var statement = root.FindNode(diagnosticSpan);
+            context.RegisterCodeFix(CodeAction.Create("Remove conditional", x => RemoveConditionalAsync(context.Document, root, statement)), diagnostic);
+        }
+
+        private Task<Solution> RemoveConditionalAsync(Document document, SyntaxNode root, SyntaxNode statement)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
