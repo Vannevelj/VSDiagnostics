3c8cf0ac0e93604986a839386f5a1eddc52d5ec5
ragnarok56 <kevin.nacios@gmail.com>
2015-05-27 21:24:07 -0400

    implement TypeToVarAnalyzer

---
+++ VSDiagnostics/VSDiagnostics/VSDiagnostics/Diagnostics/General/TypeToVar/TypeToVarCodeFix.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeActions;
+using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.CSharp;
+using System.Collections.Immutable;
+using System.Composition;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VSDiagnostics.Diagnostics.General.TypeToVar
+{
+    [ExportCodeFixProvider("TypeToVar", LanguageNames.CSharp), Shared]
+    public class TypeToVarCodeFix : CodeFixProvider
+    {
+        public override ImmutableArray<string> FixableDiagnosticIds => ImmutableArray.Create(TypeToVarAnalyzer.DiagnosticId);
+
+        public override FixAllProvider GetFixAllProvider()
+        {
+            return WellKnownFixAllProviders.BatchFixer;
+        }
+
+        public override async Task RegisterCodeFixesAsync(CodeFixContext context)
+        {
+            var root = await context.Document.GetSyntaxRootAsync(context.CancellationToken).ConfigureAwait(false);
+            var diagnostic = context.Diagnostics.First();
+            var diagnosticSpan = diagnostic.Location.SourceSpan;
+
+            var statement = root.FindNode(diagnosticSpan);
+            context.RegisterCodeFix(CodeAction.Create("Use var", x => UseVarAsync(context.Document, root, statement)), diagnostic);
+        }
+
+        public Task<Solution> UseVarAsync(Document document, SyntaxNode root, SyntaxNode statement)
+        {
+            var varIdentifier = SyntaxFactory.IdentifierName("var")
+                .WithLeadingTrivia(statement.GetLeadingTrivia())
+                .WithTrailingTrivia(statement.GetTrailingTrivia());
+
+            var newRoot = root.ReplaceNode(statement, varIdentifier);
+
+            var newDocument = document.WithSyntaxRoot(newRoot);
+            return Task.FromResult(newDocument.Project.Solution);
+        }
+    }
+}
