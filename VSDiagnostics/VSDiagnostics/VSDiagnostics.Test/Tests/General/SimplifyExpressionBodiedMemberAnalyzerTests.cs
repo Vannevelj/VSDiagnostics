3366170ffbed83077d841ae8b6ed41a3f928a859
Vannevelj <jer_vannevel@hotmail.com>
2015-05-29 23:21:35 +0200

    Added initial tests and analyzer structure

---
+++ VSDiagnostics/VSDiagnostics/VSDiagnostics.Test/Tests/General/SimplifyExpressionBodiedMemberAnalyzerTests.cs
@@ -0,0 +1,237 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RoslynTester.DiagnosticResults;
+using RoslynTester.Helpers;
+using VSDiagnostics.Diagnostics.General.SimplifyExpressionBodiedMember;
+
+namespace VSDiagnostics.Test.Tests.General
+{
+    [TestClass]
+    public class SimplifyExpressionBodiedMemberAnalyzerTests : CodeFixVerifier
+    {
+        [TestMethod]
+        public void SimplifyExpressionBodiedMemberAnalyzer_WithSimpleReturnMethod_InvokesWarning()
+        {
+            var original = @"
+using System;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class MyClass
+    {
+        int Method()
+        {
+            return 5;
+        }
+    }
+}";
+
+            var expected = @"
+using System;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class MyClass
+    {
+        int Method() => 5;
+    }
+}";
+
+            var expectedDiagnostic = new DiagnosticResult
+            {
+                Id = SimplifyExpressionBodiedMemberAnalyzer.DiagnosticId,
+                Message = SimplifyExpressionBodiedMemberAnalyzer.Message,
+                Severity = SimplifyExpressionBodiedMemberAnalyzer.Severity,
+                Locations =
+                    new[]
+                    {
+                        new DiagnosticResultLocation("Test0.cs", 13, 29)
+                    }
+            };
+
+            VerifyCSharpDiagnostic(original, expectedDiagnostic);
+            //VerifyCSharpFix(original, expected);
+        }
+
+        [TestMethod]
+        public void SimplifyExpressionBodiedMemberAnalyzer_WithSimpleGetterOnlyProperty_InvokesWarning()
+        {
+            var original = @"
+using System;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class MyClass
+    {
+        string MyProperty { get { return ""myString""; } }
+    }
+}";
+
+            var expected = @"
+using System;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class MyClass
+    {
+        string MyProperty => ""myString"";
+    }
+}";
+
+            var expectedDiagnostic = new DiagnosticResult
+            {
+                Id = SimplifyExpressionBodiedMemberAnalyzer.DiagnosticId,
+                Message = SimplifyExpressionBodiedMemberAnalyzer.Message,
+                Severity = SimplifyExpressionBodiedMemberAnalyzer.Severity,
+                Locations =
+                    new[]
+                    {
+                        new DiagnosticResultLocation("Test0.cs", 13, 29)
+                    }
+            };
+
+            VerifyCSharpDiagnostic(original, expectedDiagnostic);
+            //VerifyCSharpFix(original, expected);
+        }
+
+        [TestMethod]
+        public void SimplifyExpressionBodiedMemberAnalyzer_WithMultiLineGetterOnlyProperty_InvokesWarning()
+        {
+            var original = @"
+using System;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class MyClass
+    {
+        string MyProperty 
+        { 
+            get 
+            { 
+                return ""myString""; 
+            } 
+        }
+    }
+}";
+
+            var expected = @"
+using System;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class MyClass
+    {
+        string MyProperty => ""myString"";
+    }
+}";
+
+            var expectedDiagnostic = new DiagnosticResult
+            {
+                Id = SimplifyExpressionBodiedMemberAnalyzer.DiagnosticId,
+                Message = SimplifyExpressionBodiedMemberAnalyzer.Message,
+                Severity = SimplifyExpressionBodiedMemberAnalyzer.Severity,
+                Locations =
+                    new[]
+                    {
+                        new DiagnosticResultLocation("Test0.cs", 13, 29)
+                    }
+            };
+
+            VerifyCSharpDiagnostic(original, expectedDiagnostic);
+            //VerifyCSharpFix(original, expected);
+        }
+
+        [TestMethod]
+        public void SimplifyExpressionBodiedMemberAnalyzer_WithMultiStatementGetterOnlyProperty_DoesNotInvokeWarning()
+        {
+            var original = @"
+using System;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class MyClass
+    {
+        string MyProperty 
+        { 
+            get 
+            {
+                var newValue = ""myString"" + "".AnotherString"";
+                return newValue; 
+            } 
+        }
+    }
+}";
+            VerifyCSharpDiagnostic(original);
+        }
+
+        [TestMethod]
+        public void SimplifyExpressionBodiedMemberAnalyzer_WithMultiStatementMethod_DoesNotInvokeWarning()
+        {
+            var original = @"
+using System;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class MyClass
+    {
+        int Method()
+        {
+            var result = 5 * 5;
+            return result;
+        }
+    }
+}";
+            VerifyCSharpDiagnostic(original);
+        }
+
+        [TestMethod]
+        public void SimplifyExpressionBodiedMemberAnalyzer_WithMethodWithVoidReturn_DoesNotInvokeWarning()
+        {
+            var original = @"
+using System;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class MyClass
+    {
+        void Method()
+        {
+            var result = 5 * 5;
+        }
+    }
+}";
+            VerifyCSharpDiagnostic(original);
+        }
+
+        [TestMethod]
+        public void SimplifyExpressionBodiedMemberAnalyzer_WithMethodWithoutReturn_DoesNotInvokeWarning()
+        {
+            var original = @"
+using System;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class MyClass
+    {
+        int Method()
+        {
+            while(true)
+            {
+                Console.WriteLine(""This is a weird feature"");
+            }
+        }
+    }
+}";
+            VerifyCSharpDiagnostic(original);
+        }
+    }
+}
---
+++ VSDiagnostics/VSDiagnostics/VSDiagnostics/Diagnostics/General/SimplifyExpressionBodiedMember/SimplifyExpressionBodiedMemberAnalyzer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Immutable;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace VSDiagnostics.Diagnostics.General.SimplifyExpressionBodiedMember
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class SimplifyExpressionBodiedMemberAnalyzer : DiagnosticAnalyzer
+    {
+        public const string DiagnosticId = nameof(SimplifyExpressionBodiedMemberAnalyzer);
+        internal const string Title = "Simplify the expression using an expression-bodied member.";
+        internal const string Message = "{0} {1} can be written using an expression-bodied member";
+        internal const string Category = "General";
+        internal const DiagnosticSeverity Severity = DiagnosticSeverity.Warning;
+        internal static DiagnosticDescriptor Rule = new DiagnosticDescriptor(DiagnosticId, Title, Message, Category, Severity, true);
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(Rule);
+
+        public override void Initialize(AnalysisContext context)
+        {
+            context.RegisterSyntaxNodeAction(AnalyzeSymbol, SyntaxKind.LocalDeclarationStatement);
+        }
+
+        private void AnalyzeSymbol(SyntaxNodeAnalysisContext context)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
