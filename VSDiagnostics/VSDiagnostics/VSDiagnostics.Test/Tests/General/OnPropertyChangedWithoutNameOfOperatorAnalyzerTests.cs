95f2078f44667dcd5644fff64d00a6a0062300ae
Vannevelj <jer_vannevel@hotmail.com>
2015-05-29 18:08:45 +0200

    Added unit tests and standard analyzer stuff

---
+++ VSDiagnostics/VSDiagnostics/VSDiagnostics.Test/Tests/General/OnPropertyChangedWithoutNameOfOperatorAnalyzerTests.cs
@@ -0,0 +1,224 @@
+﻿using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RoslynTester.DiagnosticResults;
+using RoslynTester.Helpers;
+using VSDiagnostics.Diagnostics.General.OnPropertyChangedWithoutNameOfOperator;
+
+namespace VSDiagnostics.Test.Tests.General
+{
+    [TestClass]
+    public class OnPropertyChangedWithoutNameOfOperatorAnalyzerTests : CodeFixVerifier
+    {
+        [TestMethod]
+        public void OnPropertyChangedWithoutNameOfOperator_WithIdenticalString_InvokesWarning()
+        {
+            var original = @"
+using System;
+using System.Text;
+using System.ComponentModel;
+
+namespace ConsoleApplication1
+{
+    class MyClass : INotifyPropertyChanged
+    {
+        private bool _isEnabled;
+        public bool IsEnabled
+        {
+            get { return _isEnabled; }
+            set
+            {
+                _isEnabled = value;
+                OnPropertyChanged(""IsEnabled"");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+
+            if(handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }                
+    }
+}";
+
+            var expectedDiagnostic = new DiagnosticResult
+            {
+                Id = OnPropertyChangedWithoutNameOfOperatorAnalyzer.DiagnosticId,
+                Message = OnPropertyChangedWithoutNameOfOperatorAnalyzer.Message,
+                Severity = OnPropertyChangedWithoutNameOfOperatorAnalyzer.Severity,
+                Locations =
+                    new[]
+                    {
+                        new DiagnosticResultLocation("Test0.cs", 12, 30)
+                    }
+            };
+
+            VerifyCSharpDiagnostic(original, expectedDiagnostic);
+        }
+
+        [TestMethod]
+        public void OnPropertyChangedWithoutNameOfOperator_WithDifferentlyCasedString_InvokesWarning()
+        {
+            var original = @"
+using System;
+using System.Text;
+using System.ComponentModel;
+
+namespace ConsoleApplication1
+{
+    class MyClass : INotifyPropertyChanged
+    {
+        private bool _isEnabled;
+        public bool IsEnabled
+        {
+            get { return _isEnabled; }
+            set
+            {
+                _isEnabled = value;
+                OnPropertyChanged(""iSeNabled"");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+
+            if(handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }                
+    }
+}";
+
+            var expectedDiagnostic = new DiagnosticResult
+            {
+                Id = OnPropertyChangedWithoutNameOfOperatorAnalyzer.DiagnosticId,
+                Message = OnPropertyChangedWithoutNameOfOperatorAnalyzer.Message,
+                Severity = OnPropertyChangedWithoutNameOfOperatorAnalyzer.Severity,
+                Locations =
+                    new[]
+                    {
+                        new DiagnosticResultLocation("Test0.cs", 12, 30)
+                    }
+            };
+
+            VerifyCSharpDiagnostic(original, expectedDiagnostic);
+        }
+
+        [TestMethod]
+        public void OnPropertyChangedWithoutNameOfOperator_WithDifferentStringAndNoCorrespondingProperty_DoesNotInvokeWarning()
+        {
+            var original = @"
+using System;
+using System.Text;
+using System.ComponentModel;
+
+namespace ConsoleApplication1
+{
+    class MyClass : INotifyPropertyChanged
+    {
+        private bool _isEnabled;
+        public bool IsEnabled
+        {
+            get { return _isEnabled; }
+            set
+            {
+                _isEnabled = value;
+                OnPropertyChanged(""SomethingElse"");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+
+            if(handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }                
+    }
+}";
+
+            VerifyCSharpDiagnostic(original);
+        }
+
+        [TestMethod]
+        public void OnPropertyChangedWithoutNameOfOperator_WithDifferentStringAndCorrespondingProperty_InvokesWarning()
+        {
+            var original = @"
+using System;
+using System.Text;
+using System.ComponentModel;
+
+namespace ConsoleApplication1
+{
+    class MyClass : INotifyPropertyChanged
+    {
+        private bool _isEnabled;
+        public bool IsEnabled
+        {
+            get { return _isEnabled; }
+            set
+            {
+                _isEnabled = value;
+                OnPropertyChanged(""IsAnotherBoolean"");
+            }
+        }
+
+        private bool _anotherBoolean;
+        public bool IsAnotherBoolean
+        {
+            get { return _anotherBoolean; }
+            set
+            {
+                _anotherBoolean = value;
+                OnPropertyChanged(""IsAnotherBoolean"");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+
+            if(handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }                
+    }
+}";
+
+            var expectedDiagnostic = new DiagnosticResult
+            {
+                Id = OnPropertyChangedWithoutNameOfOperatorAnalyzer.DiagnosticId,
+                Message = OnPropertyChangedWithoutNameOfOperatorAnalyzer.Message,
+                Severity = OnPropertyChangedWithoutNameOfOperatorAnalyzer.Severity,
+                Locations =
+                    new[]
+                    {
+                        new DiagnosticResultLocation("Test0.cs", 12, 30)
+                    }
+            };
+
+            VerifyCSharpDiagnostic(original, expectedDiagnostic);
+        }
+
+        protected override DiagnosticAnalyzer GetCSharpDiagnosticAnalyzer()
+        {
+            return new OnPropertyChangedWithoutNameOfOperatorAnalyzer();
+        }
+    }
+}
---
+++ VSDiagnostics/VSDiagnostics/VSDiagnostics/Diagnostics/General/OnPropertyChangedWithoutNameOfOperator/OnPropertyChangedWithoutNameOfOperatorAnalyzer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace VSDiagnostics.Diagnostics.General.OnPropertyChangedWithoutNameOfOperator
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class OnPropertyChangedWithoutNameOfOperatorAnalyzer : DiagnosticAnalyzer
+    {
+        public const string DiagnosticId = nameof(OnPropertyChangedWithoutNameOfOperatorAnalyzer);
+        internal const string Title = "Use the nameof() operator in conjunection with OnPropertyChanged";
+        internal const string Message = "OnPropertyChanged({0}) can use the nameof() operator.";
+        internal const string Category = "General";
+        internal const DiagnosticSeverity Severity = DiagnosticSeverity.Warning;
+        internal static DiagnosticDescriptor Rule = new DiagnosticDescriptor(DiagnosticId, Title, Message, Category, Severity, true);
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(Rule);
+
+        public override void Initialize(AnalysisContext context)
+        {
+            context.RegisterSyntaxNodeAction(AnalyzeSymbol, SyntaxKind.InvocationExpression);
+        }
+
+        private void AnalyzeSymbol(SyntaxNodeAnalysisContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
