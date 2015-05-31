0fdfa248d2104c95457cebae253ac4f95242832a
Vannevelj <jer_vannevel@hotmail.com>
2015-05-31 19:13:20 +0200

    basic implementation of a few tests and the analyzer skeleton

---
+++ VSDiagnostics/VSDiagnostics/VSDiagnostics.Test/Tests/General/TryCastUsingAsNotNullInsteadOfIsAsAnalyzerTests.cs
@@ -0,0 +1,206 @@
+﻿using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RoslynTester.DiagnosticResults;
+using RoslynTester.Helpers;
+using VSDiagnostics.Diagnostics.General.TryCastUsingAsNotNullInsteadOfIsAs;
+
+namespace VSDiagnostics.Test.Tests.General
+{
+    [TestClass]
+    public class TryCastUsingAsNotNullInsteadOfIsAsAnalyzerTests : CodeFixVerifier
+    {
+        [TestMethod]
+        public void TryCastUsingAsNotNullInsteadOfIsAsAnalyzer_WithIsAs_AndReferenceType_InvokesWarning()
+        {
+            var original = @"
+using System;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class MyClass
+    {
+        void Method()
+        {
+            object o = ""sample"";
+            if (o is string)
+            {
+                int oAsInt = o as string;
+            }
+        }
+    }
+}";
+
+            var expectedDiagnostic = new DiagnosticResult
+            {
+                Id = TryCastUsingAsNotNullInsteadOfIsAsAnalyzer.DiagnosticId,
+                Message = string.Format(TryCastUsingAsNotNullInsteadOfIsAsAnalyzer.Message, "o"),
+                Severity = TryCastUsingAsNotNullInsteadOfIsAsAnalyzer.Severity,
+                Locations =
+                    new[]
+                    {
+                        new DiagnosticResultLocation("Test0.cs", 12, 30)
+                    }
+            };
+
+            VerifyCSharpDiagnostic(original, expectedDiagnostic);
+        }
+
+        [TestMethod]
+        public void TryCastUsingAsNotNullInsteadOfIsAsAnalyzer_WithIsAs_AndValueType_InvokesWarning()
+        {
+            var original = @"
+using System;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class MyClass
+    {
+        void Method()
+        {
+            object o = 5;
+            if (o is int)
+            {
+                var oAsInt = o as int?;
+            }
+        }
+    }
+}";
+
+            var expectedDiagnostic = new DiagnosticResult
+            {
+                Id = TryCastUsingAsNotNullInsteadOfIsAsAnalyzer.DiagnosticId,
+                Message = string.Format(TryCastUsingAsNotNullInsteadOfIsAsAnalyzer.Message, "o"),
+                Severity = TryCastUsingAsNotNullInsteadOfIsAsAnalyzer.Severity,
+                Locations =
+                    new[]
+                    {
+                        new DiagnosticResultLocation("Test0.cs", 12, 30)
+                    }
+            };
+
+            VerifyCSharpDiagnostic(original, expectedDiagnostic);
+        }
+
+        [TestMethod]
+        public void TryCastUsingAsNotNullInsteadOfIsAsAnalyzer_WithIsAs_AndObjectIsUsedBeforeIs_InvokesWarning()
+        {
+            var original = @"
+using System;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class MyClass
+    {
+        void Method()
+        {
+            object o = ""sample"";
+            Console.Write(o.GetType());
+            if (o is string)
+            {
+                int oAsInt = o as string;
+            }
+        }
+    }
+}";
+
+            var expectedDiagnostic = new DiagnosticResult
+            {
+                Id = TryCastUsingAsNotNullInsteadOfIsAsAnalyzer.DiagnosticId,
+                Message = string.Format(TryCastUsingAsNotNullInsteadOfIsAsAnalyzer.Message, "o"),
+                Severity = TryCastUsingAsNotNullInsteadOfIsAsAnalyzer.Severity,
+                Locations =
+                    new[]
+                    {
+                        new DiagnosticResultLocation("Test0.cs", 12, 30)
+                    }
+            };
+
+            VerifyCSharpDiagnostic(original, expectedDiagnostic);
+        }
+
+        [TestMethod]
+        public void TryCastUsingAsNotNullInsteadOfIsAsAnalyzer_WithIsAs_AndObjectIsMethodParameter_InvokesWarning()
+        {
+            var original = @"
+using System;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class MyClass
+    {
+        void Method(object o)
+        {
+            if (o is string)
+            {
+                int oAsInt = o as string;
+            }
+        }
+    }
+}";
+
+            var expectedDiagnostic = new DiagnosticResult
+            {
+                Id = TryCastUsingAsNotNullInsteadOfIsAsAnalyzer.DiagnosticId,
+                Message = string.Format(TryCastUsingAsNotNullInsteadOfIsAsAnalyzer.Message, "o"),
+                Severity = TryCastUsingAsNotNullInsteadOfIsAsAnalyzer.Severity,
+                Locations =
+                    new[]
+                    {
+                        new DiagnosticResultLocation("Test0.cs", 12, 30)
+                    }
+            };
+
+            VerifyCSharpDiagnostic(original, expectedDiagnostic);
+        }
+
+        [TestMethod]
+        public void TryCastUsingAsNotNullInsteadOfIsAsAnalyzer_WithIsAs_AndElseClause_InvokesWarning()
+        {
+            var original = @"
+using System;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class MyClass
+    {
+        void Method()
+        {
+            object o = ""sample"";
+            if (o is string)
+            {
+                int oAsInt = o as string;
+            }
+            else
+            {
+                Console.Write(""something"");
+            }
+        }
+    }
+}";
+
+            var expectedDiagnostic = new DiagnosticResult
+            {
+                Id = TryCastUsingAsNotNullInsteadOfIsAsAnalyzer.DiagnosticId,
+                Message = string.Format(TryCastUsingAsNotNullInsteadOfIsAsAnalyzer.Message, "o"),
+                Severity = TryCastUsingAsNotNullInsteadOfIsAsAnalyzer.Severity,
+                Locations =
+                    new[]
+                    {
+                        new DiagnosticResultLocation("Test0.cs", 12, 30)
+                    }
+            };
+
+            VerifyCSharpDiagnostic(original, expectedDiagnostic);
+        }
+
+        protected override DiagnosticAnalyzer GetCSharpDiagnosticAnalyzer()
+        {
+            return new TryCastUsingAsNotNullInsteadOfIsAsAnalyzer();
+        }
+    }
+}
---
+++ VSDiagnostics/VSDiagnostics/VSDiagnostics/Diagnostics/General/TryCastUsingAsNotNullInsteadOfIsAs/TryCastUsingAsNotNullInsteadOfIsAsAnalyzer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Immutable;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace VSDiagnostics.Diagnostics.General.TryCastUsingAsNotNullInsteadOfIsAs
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class TryCastUsingAsNotNullInsteadOfIsAsAnalyzer : DiagnosticAnalyzer
+    {
+        public const string DiagnosticId = nameof(TryCastUsingAsNotNullInsteadOfIsAsAnalyzer);
+        internal const string Title = "The conversion can be performed without casting twice.";
+        internal const string Message = "Variable {0} can be casted using as/null.";
+        internal const string Category = "General";
+        internal const DiagnosticSeverity Severity = DiagnosticSeverity.Warning;
+        internal static DiagnosticDescriptor Rule = new DiagnosticDescriptor(DiagnosticId, Title, Message, Category, Severity, true);
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(Rule);
+
+        public override void Initialize(AnalysisContext context)
+        {
+            context.RegisterSyntaxNodeAction(AnalyzeSymbol, SyntaxKind.PropertyDeclaration, SyntaxKind.MethodDeclaration);
+        }
+
+        private void AnalyzeSymbol(SyntaxNodeAnalysisContext obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
