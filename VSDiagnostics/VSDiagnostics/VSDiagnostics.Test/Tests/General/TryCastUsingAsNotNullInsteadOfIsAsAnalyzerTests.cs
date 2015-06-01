1910c3ee32d9341d367dc79624b6648e7dbccbb7
Vannevelj <jer_vannevel@hotmail.com>
2015-06-01 02:50:33 +0200

    Implemented the Code Fix, added more test cases

--- VSDiagnostics/VSDiagnostics/VSDiagnostics.Test/Tests/General/TryCastUsingAsNotNullInsteadOfIsAsAnalyzerTests.cs
+++ VSDiagnostics/VSDiagnostics/VSDiagnostics.Test/Tests/General/TryCastUsingAsNotNullInsteadOfIsAsAnalyzerTests.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.CodeAnalysis.Diagnostics;
+﻿using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.Diagnostics;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using RoslynTester.DiagnosticResults;
 using RoslynTester.Helpers;
@@ -25,7 +26,26 @@ namespace ConsoleApplication1
             object o = ""sample"";
             if (o is string)
             {
-                int oAsInt = o as string;
+                string oAsString = o as string;
+            }
+        }
+    }
+}";
+
+            var expected = @"
+using System;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class MyClass
+    {
+        void Method()
+        {
+            object o = ""sample"";
+            string oAsString = o as string;
+            if (oAsString != null)
+            {
             }
         }
     }
@@ -44,6 +64,7 @@ namespace ConsoleApplication1
             };
 
             VerifyCSharpDiagnostic(original, expectedDiagnostic);
+            VerifyCSharpFix(original, expected);
         }
 
         [TestMethod]
@@ -68,6 +89,26 @@ namespace ConsoleApplication1
     }
 }";
 
+            var expected = @"
+using System;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class MyClass
+    {
+        void Method()
+        {
+            object o = 5;
+            var oAsInt = o as int?;
+            if (oAsInt != null)
+            {
+            }
+        }
+    }
+}";
+
+
             var expectedDiagnostic = new DiagnosticResult
             {
                 Id = TryCastWithoutUsingAsNotNullAnalyzer.DiagnosticId,
@@ -81,6 +122,7 @@ namespace ConsoleApplication1
             };
 
             VerifyCSharpDiagnostic(original, expectedDiagnostic);
+            VerifyCSharpFix(original, expected);
         }
 
         [TestMethod]
@@ -100,7 +142,27 @@ namespace ConsoleApplication1
             Console.Write(o.GetType());
             if (o is string)
             {
-                int oAsInt = o as string;
+                string oAsString = o as string;
+            }
+        }
+    }
+}";
+
+            var expected = @"
+using System;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class MyClass
+    {
+        void Method()
+        {
+            object o = ""sample"";
+            Console.Write(o.GetType());
+            string oAsString = o as string;
+            if (oAsString != null)
+            {
             }
         }
     }
@@ -119,6 +181,7 @@ namespace ConsoleApplication1
             };
 
             VerifyCSharpDiagnostic(original, expectedDiagnostic);
+            VerifyCSharpFix(original, expected);
         }
 
         [TestMethod]
@@ -136,7 +199,25 @@ namespace ConsoleApplication1
         {
             if (o is string)
             {
-                int oAsInt = o as string;
+                string oAsString = o as string;
+            }
+        }
+    }
+}";
+
+            var expected = @"
+using System;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class MyClass
+    {
+        void Method(object o)
+        {
+            string oAsString = o as string;
+            if (oAsString != null)
+            {
             }
         }
     }
@@ -155,6 +236,7 @@ namespace ConsoleApplication1
             };
 
             VerifyCSharpDiagnostic(original, expectedDiagnostic);
+            VerifyCSharpFix(original, expected);
         }
 
         [TestMethod]
@@ -173,7 +255,30 @@ namespace ConsoleApplication1
             object o = ""sample"";
             if (o is string)
             {
-                int oAsInt = o as string;
+                string oAsString = o as string;
+            }
+            else
+            {
+                Console.Write(""something"");
+            }
+        }
+    }
+}";
+
+            var expected = @"
+using System;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class MyClass
+    {
+        void Method()
+        {
+            object o = ""sample"";
+            string oAsString = o as string;
+            if (oAsString != null)
+            {
             }
             else
             {
@@ -196,6 +301,7 @@ namespace ConsoleApplication1
             };
 
             VerifyCSharpDiagnostic(original, expectedDiagnostic);
+            VerifyCSharpFix(original, expected);
         }
 
         [TestMethod]
@@ -222,6 +328,27 @@ namespace ConsoleApplication1
     }
 }";
 
+            var expected = @"
+using System;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class MyClass
+    {
+        void Method()
+        {
+            object o = 5;
+            var oAsInt = o as int?;
+            if (oAsInt != null)
+            {
+                object irrelevant = 10.0;
+                var irrelevantAsDouble = irrelevant as double?;
+            }
+        }
+    }
+}";
+
             var expectedDiagnostic = new DiagnosticResult
             {
                 Id = TryCastWithoutUsingAsNotNullAnalyzer.DiagnosticId,
@@ -235,6 +362,7 @@ namespace ConsoleApplication1
             };
 
             VerifyCSharpDiagnostic(original, expectedDiagnostic);
+            VerifyCSharpFix(original, expected);
         }
 
         [TestMethod]
@@ -300,9 +428,73 @@ namespace ConsoleApplication1
             VerifyCSharpDiagnostic(original);
         }
 
+        [TestMethod]
+        public void TryCastWithoutUsingAsNotNullAnalyzer_WithChainedVariableDeclaration_InvokesWarning()
+        {
+            var original = @"
+using System;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class MyClass
+    {
+        void Method()
+        {
+            object o = 5;
+            if (o is int)
+            {
+                int? oAsInt = o as int?, x = 10;
+            }
+        }
+    }
+}";
+
+            var expected = @"
+using System;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class MyClass
+    {
+        void Method()
+        {
+            object o = 5;
+            int oAsInt = o as int?;
+            if (oAsInt != null)
+            {
+                int? x = 10;
+            }
+        }
+    }
+}";
+
+
+            var expectedDiagnostic = new DiagnosticResult
+            {
+                Id = TryCastWithoutUsingAsNotNullAnalyzer.DiagnosticId,
+                Message = string.Format(TryCastWithoutUsingAsNotNullAnalyzer.Message, "o"),
+                Severity = TryCastWithoutUsingAsNotNullAnalyzer.Severity,
+                Locations =
+                    new[]
+                    {
+                        new DiagnosticResultLocation("Test0.cs", 12, 17)
+                    }
+            };
+
+            VerifyCSharpDiagnostic(original, expectedDiagnostic);
+            VerifyCSharpFix(original, expected);
+        }
+
         protected override DiagnosticAnalyzer GetCSharpDiagnosticAnalyzer()
         {
             return new TryCastWithoutUsingAsNotNullAnalyzer();
+        }
+
+        protected override CodeFixProvider GetCSharpCodeFixProvider()
+        {
+            return new TryCastWithoutUsingAsNotNullCodeFix();
         }
     }
 }
---
+++ VSDiagnostics/VSDiagnostics/VSDiagnostics/Diagnostics/General/TryCastWithoutUsingAsNotNull/TryCastWithoutUsingAsNotNullCodeFix.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections.Immutable;
+using System.Composition;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeActions;
+using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Editing;
+using Microsoft.CodeAnalysis.Formatting;
+
+namespace VSDiagnostics.Diagnostics.General.TryCastWithoutUsingAsNotNull
+{
+    [ExportCodeFixProvider("TryCastWithoutUsingAsNotNull", LanguageNames.CSharp), Shared]
+    public class TryCastWithoutUsingAsNotNullCodeFix : CodeFixProvider
+    {
+        public override ImmutableArray<string> FixableDiagnosticIds => ImmutableArray.Create(TryCastWithoutUsingAsNotNullAnalyzer.DiagnosticId);
+        public override FixAllProvider GetFixAllProvider() => WellKnownFixAllProviders.BatchFixer;
+
+        public override async Task RegisterCodeFixesAsync(CodeFixContext context)
+        {
+            var root = await context.Document.GetSyntaxRootAsync(context.CancellationToken).ConfigureAwait(false);
+            var diagnostic = context.Diagnostics.First();
+            var diagnosticSpan = diagnostic.Location.SourceSpan;
+
+            var statement = root.FindNode(diagnosticSpan);
+            context.RegisterCodeFix(CodeAction.Create("Use as", x => UseAs(context.Document, root, statement)), diagnostic);
+        }
+
+        private async Task<Solution> UseAs(Document document, SyntaxNode root, SyntaxNode statement)
+        {
+            var isExpression = (BinaryExpressionSyntax) statement;
+            var ifStatement = statement.AncestorsAndSelf().OfType<IfStatementSyntax>().First();
+
+            var asExpressions = ifStatement.Statement.DescendantNodesAndSelf().OfType<BinaryExpressionSyntax>().Where(x => x.OperatorToken.IsKind(SyntaxKind.AsKeyword));
+            var isIdentifier = ((IdentifierNameSyntax) isExpression.Left).Identifier.ValueText;
+
+            foreach (var asExpression in asExpressions)
+            {
+                var asIdentifier = ((IdentifierNameSyntax) asExpression.Left).Identifier.ValueText;
+                if (string.Equals(isIdentifier, asIdentifier))
+                {
+                    // Move the as statement before the if block
+                    // Change the if-condition to "NewAsIdentifier != null"
+
+                    var variableDeclarator = asExpression.AncestorsAndSelf().OfType<VariableDeclaratorSyntax>().First();
+                    var newAsIdentifier = variableDeclarator.Identifier.ValueText;
+                    var newCondition = SyntaxFactory.ParseExpression($"{newAsIdentifier} != null");
+
+                    var variableDeclaration = asExpression.AncestorsAndSelf().OfType<LocalDeclarationStatementSyntax>().First();
+
+                    var editor = await DocumentEditor.CreateAsync(document);
+                    editor.RemoveNode(variableDeclaration);
+                    editor.ReplaceNode(ifStatement.Condition, newCondition);
+                    editor.InsertBefore(ifStatement, new[] { variableDeclaration.WithAdditionalAnnotations(Formatter.Annotation) });
+
+                    var newDocument = editor.GetChangedDocument();
+                    return newDocument.Project.Solution;
+                }
+            }
+
+            return null;
+        }
+    }
+}
