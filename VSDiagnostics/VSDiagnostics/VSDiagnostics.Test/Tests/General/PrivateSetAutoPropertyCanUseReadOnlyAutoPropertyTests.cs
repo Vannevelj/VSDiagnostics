73fe8884be92d6dfff57de49e96516c89b77b198
Christopher J. McClellan <ckuhn203@gmail.com>
2015-06-02 14:50:54 -0400

    stashing

---
+++ VSDiagnostics/VSDiagnostics/VSDiagnostics.Test/Tests/General/PrivateSetAutoPropertyCanUseReadOnlyAutoPropertyTests.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RoslynTester.DiagnosticResults;
+using RoslynTester.Helpers;
+using VSDiagnostics.Diagnostics.General.PrivateSetAutoPropertyCanBeReadOnlyAutoProperty;
+
+namespace VSDiagnostics.Test.Tests.General
+{
+    [TestClass]
+    public class PrivateSetAutoPropertyCanUseReadOnlyAutoPropertyTests : CodeFixVerifier
+    {
+        [TestMethod]
+        public void PrivateSetAutoPropertyCanUseReadOnlyAutoPropertyAnalyzer_WithConstructorAccessOnly_InvokesWarning()
+        {
+            var original = @"
+using System;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class MyClass
+    {
+        int SomeProperty {get; private set;}
+   
+        MyClass()
+        {
+            this.SomeProperty = 42;
+        }
+
+        void SomeMethod()
+        {
+            bool isAwesome = true;
+            if (isAwesome)
+            {
+                Console.WriteLine(""awesome"");
+            }
+        }
+    }
+}";
+            var expectedDiagnostic = new DiagnosticResult
+            {
+                Id = PrivateSetAutoPropertyCanBeReadOnlyAutoPropertyAnalyzer.DiagnosticId,
+                Message = PrivateSetAutoPropertyCanBeReadOnlyAutoPropertyAnalyzer.Message,
+                Severity = PrivateSetAutoPropertyCanBeReadOnlyAutoPropertyAnalyzer.Severity,
+
+                Locations =
+                        new[]
+                        {
+                            new DiagnosticResultLocation("Test0.cs", 8, 31)
+                        }
+            };
+
+            
+            VerifyCSharpDiagnostic(original, expectedDiagnostic);
+            //todo: create fix
+            //VerifyCSharpFix(original, result);
+        }
+
+        [TestMethod]
+        public void PrivateSetAutoPropertyCanUseReadOnlyAutoPropertyAnalyzer_WithMethodAccess_DoesNotInvokesWarning()
+        {
+
+        }
+    }
+}
---
+++ VSDiagnostics/VSDiagnostics/VSDiagnostics/Diagnostics/General/PrivateSetAutoPropertyCanBeReadOnlyAutoProperty/PrivateSetAutoPropertyCanBeReadOnlyAutoPropertyAnalyzer.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Immutable;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+
+namespace VSDiagnostics.Diagnostics.General.PrivateSetAutoPropertyCanBeReadOnlyAutoProperty
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class PrivateSetAutoPropertyCanBeReadOnlyAutoPropertyAnalyzer : DiagnosticAnalyzer
+    {
+        public const string DiagnosticId = nameof(PrivateSetAutoPropertyCanBeReadOnlyAutoPropertyAnalyzer);
+        internal const string Title = "Private Set AutoProperty is only set once. It can be made readonly.";
+        internal const string Message = "Private Set can be made ReadOnly";
+        internal const string Category = "General";
+        internal const DiagnosticSeverity Severity = DiagnosticSeverity.Warning;
+        internal static DiagnosticDescriptor Rule = new DiagnosticDescriptor(DiagnosticId, Title, Message, Category, Severity, true);
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(Rule);
+
+        public override void Initialize(AnalysisContext context)
+        {
+            context.RegisterSyntaxNodeAction(AnalyzeSymbol, SyntaxKind.PropertyDeclaration, SyntaxKind.ConstructorDeclaration);
+        }
+
+        private void AnalyzeSymbol(SyntaxNodeAnalysisContext context)
+        {
+            Diagnostic diagnostic = null;
+
+            var asProperty = context.Node as PropertyDeclarationSyntax;
+            if (asProperty != null)
+            {
+                diagnostic = HandleProperty(asProperty);
+            }
+
+            var asConstructor = context.Node as ConstructorDeclarationSyntax;
+            if (asConstructor != null)
+            {
+                diagnostic = HandleConstructor(asConstructor);
+            }
+
+            //var asMethod = context.Node as MethodDeclarationSyntax;
+            //if (asMethod != null)
+            //{
+            //    diagnostic = HandleMethod(asMethod);
+            //}
+
+            if (diagnostic != null)
+            {
+                context.ReportDiagnostic(diagnostic);
+            }
+        }
+
+        private Diagnostic HandleConstructor(ConstructorDeclarationSyntax asConstructor)
+        {
+            // throw new NotImplementedException();
+            return null;
+        }
+
+        private Diagnostic HandleProperty(PropertyDeclarationSyntax propertyDeclaration)
+        {
+            if (propertyDeclaration.ExpressionBody != null)
+            {
+                return null;
+            }
+
+            var setter = propertyDeclaration.AccessorList.Accessors;
+
+            return null;
+        }
+    }
+}
