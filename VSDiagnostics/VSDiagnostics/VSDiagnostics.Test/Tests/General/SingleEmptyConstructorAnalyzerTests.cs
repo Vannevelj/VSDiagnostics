8fccdcbe2b9190c3fc067716a6eaf2214234ee28
Hosch250 <hosch250@live.com>
2015-08-05 12:58:14 -0500

    Close issue #20 - Find and remove public empty ctor's with no comments.

---
+++ VSDiagnostics/VSDiagnostics/VSDiagnostics.Test/Tests/General/SingleEmptyConstructorAnalyzerTests.cs
@@ -0,0 +1,137 @@
+﻿using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RoslynTester.Helpers.CSharp;
+using VSDiagnostics.Diagnostics.General.SingleEmptyConstructor;
+
+namespace VSDiagnostics.Test.Tests.General
+{
+    [TestClass]
+    public class SingleEmptyConstructorAnalyzerTests : CSharpCodeFixVerifier
+    {
+        protected override DiagnosticAnalyzer DiagnosticAnalyzer => new SingleEmptyConstructorAnalyzer();
+
+        protected override CodeFixProvider CodeFixProvider => new SingleEmptyConstructorCodeFix();
+
+        [TestMethod]
+        public void SingleEmptyConstructorAnalyzer_WithEmptyConstructor_InvokesWarning ()
+        {
+            var original = @"
+namespace ConsoleApplication1
+{
+    public class MyClass
+    {
+        public MyClass()
+        {
+        }
+    }
+}";
+
+            var result = @"
+namespace ConsoleApplication1
+{
+    public class MyClass
+    {
+    }
+}";
+
+            VerifyDiagnostic (original, SingleEmptyConstructorAnalyzer.Rule.MessageFormat.ToString());
+            VerifyFix(original, result);
+        }
+
+        [TestMethod]
+        public void SingleEmptyConstructorAnalyzer_WithCommentInConstructor_DoesNotInvokeWarning ()
+        {
+            var original = @"
+namespace ConsoleApplication1
+{
+    public class MyClass
+    {
+        public MyClass()
+        {
+            // ctor has comment
+        }
+    }
+}";
+
+            VerifyDiagnostic(original);
+        }
+
+        [TestMethod]
+        public void SingleEmptyConstructorAnalyzer_WithConstructorParameters_DoesNotInvokeWarning ()
+        {
+            var original = @"
+namespace ConsoleApplication1
+{
+    public class MyClass
+    {
+        public MyClass(int foo)
+        {
+        }
+    }
+}";
+
+            VerifyDiagnostic(original);
+        }
+
+        [TestMethod]
+        public void SingleEmptyConstructorAnalyzer_WithConstructorBody_DoesNotInvokeWarning ()
+        {
+            var original = @"
+namespace ConsoleApplication1
+{
+    public class MyClass
+    {
+        public int Foo { get; }
+
+        public MyClass()
+        {
+            Foo = 0;
+        }
+    }
+}";
+
+            VerifyDiagnostic(original);
+        }
+
+        [TestMethod]
+        public void SingleEmptyConstructorAnalyzer_WithImplicitPrivateConstructor_DoesNotInvokeWarning ()
+        {
+            var original = @"
+namespace ConsoleApplication1
+{
+    public class MyClass
+    {
+        public int Foo { get; }
+
+        MyClass()
+        {
+            Foo = 0;
+        }
+    }
+}";
+
+            VerifyDiagnostic(original);
+        }
+
+        [TestMethod]
+        public void SingleEmptyConstructorAnalyzer_WithExplicitInternalConstructor_DoesNotInvokeWarning ()
+        {
+            var original = @"
+namespace ConsoleApplication1
+{
+    public class MyClass
+    {
+        public int Foo { get; }
+
+        internal MyClass()
+        {
+            Foo = 0;
+        }
+    }
+}";
+
+            VerifyDiagnostic(original);
+        }
+    }
+}
---
+++ VSDiagnostics/VSDiagnostics/VSDiagnostics/Diagnostics/General/SingleEmptyConstructor/SingleEmptyConstructorAnalyzer.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections.Immutable;
+using System.Linq;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace VSDiagnostics.Diagnostics.General.SingleEmptyConstructor
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    class SingleEmptyConstructorAnalyzer : DiagnosticAnalyzer
+    {
+        private const string Category = "General";
+        private const string DiagnosticId = nameof(SingleEmptyConstructorAnalyzer);
+        private const string Message = "Use default constructor.";
+        private const DiagnosticSeverity Severity = DiagnosticSeverity.Warning;
+        private const string Title = "Your constructor is the same as a default constructor and can be removed.";
+
+        internal static DiagnosticDescriptor Rule => new DiagnosticDescriptor(DiagnosticId, Title, Message, Category, Severity, true);
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(Rule);
+
+        public override void Initialize (AnalysisContext context)
+        {
+            context.RegisterSyntaxNodeAction(AnalyzeSymbol, SyntaxKind.ConstructorDeclaration);
+        }
+
+        private void AnalyzeSymbol (SyntaxNodeAnalysisContext context)
+        {
+            var ctorExpression = context.Node as ConstructorDeclarationSyntax;
+            if (ctorExpression == null)
+            {
+                return;
+            }
+            
+            // ctor must be public
+            if (ctorExpression.Modifiers.ToImmutableList().All(m => m.Text != "public"))
+            {
+                return;
+            }
+
+            // ctor must not have parameters
+            if (ctorExpression.ParameterList.Parameters.Any())
+            {
+                return;
+            }
+
+            // ctor must have no body statements
+            if (ctorExpression.Body.Statements.Any())
+            {
+                return;
+            }
+
+            // ctor must not have comment - ignore empty brace pairs
+            var text = ctorExpression.Body.GetText().ToString().ToCharArray();
+            if (text.Any(ch => !char.IsWhiteSpace(ch) && ch != '{' && ch != '}'))
+            {
+                return;
+            }
+
+            context.ReportDiagnostic(Diagnostic.Create(Rule, ctorExpression.GetLocation()));
+        }
+    }
+}
