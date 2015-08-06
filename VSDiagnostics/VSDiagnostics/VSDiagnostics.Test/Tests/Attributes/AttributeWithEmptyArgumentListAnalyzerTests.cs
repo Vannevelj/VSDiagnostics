67c6973d08c6c452aebb1fa6961be69d7b0fee67
Hosch250 <hosch250@live.com>
2015-08-06 12:53:13 -0500

    Close #110.  Implement check and fix for attributes with empty argument lists.

---
+++ VSDiagnostics/VSDiagnostics/VSDiagnostics.Test/Tests/Attributes/AttributeWithEmptyArgumentListAnalyzerTests.cs
@@ -0,0 +1,151 @@
+﻿using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RoslynTester.Helpers.CSharp;
+using VSDiagnostics.Diagnostics.Attributes.AttributeWithEmptyArgumentList;
+
+namespace VSDiagnostics.Test.Tests.Attributes
+{
+    [TestClass]
+    public class AttributeWithEmptyArgumentListAnalyzerTests : CSharpCodeFixVerifier
+    {
+        protected override DiagnosticAnalyzer DiagnosticAnalyzer => new AttributeWithEmptyArgumentListAnalyzer();
+
+        protected override CodeFixProvider CodeFixProvider => new AttributeWithEmptyArgumentListCodeFix();
+
+        [TestMethod]
+        public void AttributeWithEmptyArgumentListAnalyzer_AttributeWithEmptyArgumentList_InvokesWarning()
+        {
+            var original = @"
+using System;
+
+namespace ConsoleApplication1
+{
+    class MyClass
+    {   
+        [Obsolete()]
+        void Method(string input)
+        {
+        }
+    }
+}";
+
+            var result = @"
+using System;
+
+namespace ConsoleApplication1
+{
+    class MyClass
+    {   
+        [Obsolete]
+        void Method(string input)
+        {
+        }
+    }
+}";
+
+            VerifyDiagnostic(original, AttributeWithEmptyArgumentListAnalyzer.Rule.MessageFormat.ToString());
+            VerifyFix(original, result);
+        }
+
+        [TestMethod]
+        public void AttributeWithEmptyArgumentListAnalyzer_WithoutArgumentList_DoesNotInvokeWarning()
+        {
+            var original = @"
+using System;
+
+namespace ConsoleApplication1
+{
+    class MyClass
+    {   
+        [Obsolete]
+        void Method(string input)
+        {
+        }
+    }
+}";
+
+            VerifyDiagnostic(original);
+        }
+
+        [TestMethod]
+        public void AttributeWithEmptyArgumentListAnalyzer_WithArgumentList_DoesNotInvokeWarning()
+        {
+            var original = @"
+using System;
+
+namespace ConsoleApplication1
+{
+    class MyClass
+    {   
+        [Obsolete(""test"", true)]
+        void Method(string input)
+        {
+        }
+    }
+}";
+
+            VerifyDiagnostic(original);
+        }
+
+        // make sure it works on other attributes besides [Obsolete]
+        [TestMethod]
+        public void AttributeWithEmptyArgumentListAnalyzer_AttributeWithEmptyArgumentList_FlagsAttribute_InvokesWarning()
+        {
+            var original = @"
+using System;
+
+namespace ConsoleApplication1
+{
+    class MyClass
+    {   
+        [Flag()]
+        enum Foo
+        {
+            Bar, Baz
+        }
+    }
+}";
+
+            var result = @"
+using System;
+
+namespace ConsoleApplication1
+{
+    class MyClass
+    {   
+        [Flag]
+        enum Foo
+        {
+            Bar, Baz
+        }
+    }
+}";
+
+            VerifyDiagnostic(original, AttributeWithEmptyArgumentListAnalyzer.Rule.MessageFormat.ToString());
+            VerifyFix(original, result);
+        }
+
+        // make sure it works on other attributes besides [Obsolete]
+        [TestMethod]
+        public void AttributeWithEmptyArgumentListAnalyzer_WithoutArgumentList_FlagsAttribute_DoesNotInvokeWarning()
+        {
+            var original = @"
+using System;
+
+namespace ConsoleApplication1
+{
+    class MyClass
+    {   
+        [Flag]
+        enum Foo
+        {
+            Bar, Baz
+        }
+    }
+}";
+
+            VerifyDiagnostic(original);
+        }
+    }
+}
