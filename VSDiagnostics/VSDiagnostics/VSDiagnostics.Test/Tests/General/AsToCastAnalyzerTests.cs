003065d269cb20606fd05d27d7d7e24761fd330b
Hosch250 <hosch250@live.com>
2015-08-06 15:23:46 -0500

    Close #51 - AsToCast analyzer and code fix.

---
+++ VSDiagnostics/VSDiagnostics/VSDiagnostics.Test/Tests/General/AsToCastAnalyzerTests.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RoslynTester.Helpers.CSharp;
+using VSDiagnostics.Diagnostics.General.AsToCast;
+
+namespace VSDiagnostics.Test.Tests.General
+{
+    [TestClass]
+    public class AsToCastAnalyzerTests : CSharpCodeFixVerifier
+    {
+        protected override DiagnosticAnalyzer DiagnosticAnalyzer => new AsToCastAnalyzer();
+
+        protected override CodeFixProvider CodeFixProvider => new AsToCastCodeFix();
+
+        [TestMethod]
+        public void AsToCastAnalyzer_PredefinedType_InvokesWarning()
+        {
+            var original = @"
+namespace ConsoleApplication1
+{
+    class MyClass
+    {
+        void Method()
+        {
+            var ch = 'r';
+            var i = ch as int;
+        }
+    }
+}";
+
+            var result = @"
+namespace ConsoleApplication1
+{
+    class MyClass
+    {
+        void Method()
+        {
+            var ch = 'r';
+            var i = (int)ch;
+        }
+    }
+}";
+
+            VerifyDiagnostic(original, AsToCastAnalyzer.Rule.MessageFormat.ToString());
+            VerifyFix(original, result);
+        }
+
+        [TestMethod]
+        public void AsToCastAnalyzer_CustomType_InvokesWarning()
+        {
+            var original = @"
+namespace ConsoleApplication1
+{
+    interface P
+    {
+    }
+
+    class Program : P
+    {
+    }
+
+    class MyClass
+    {
+        void Method()
+        {
+            P variable = new Program();
+            var i = ch as Program;
+        }
+    }
+}";
+
+            var result = @"
+namespace ConsoleApplication1
+{
+    interface P
+    {
+    }
+
+    class Program : P
+    {
+    }
+
+    class MyClass
+    {
+        void Method()
+        {
+            P variable = new Program();
+            var i = (Program)ch;
+        }
+    }
+}";
+
+            VerifyDiagnostic(original, AsToCastAnalyzer.Rule.MessageFormat.ToString());
+            VerifyFix(original, result);
+        }
+    }
+}
---
+++ VSDiagnostics/VSDiagnostics/VSDiagnostics/Diagnostics/General/AsToCast/AsToCastAnalyzer.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Immutable;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace VSDiagnostics.Diagnostics.General.AsToCast
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class AsToCastAnalyzer : DiagnosticAnalyzer
+    {
+        private const string Category = "General";
+        private const string DiagnosticId = nameof(AsToCastAnalyzer);
+        private const string Message = "Use cast instead of as.";
+        private const DiagnosticSeverity Severity = DiagnosticSeverity.Warning;
+        private const string Title = "You can use a cast isntead of as.";
+
+        internal static DiagnosticDescriptor Rule => new DiagnosticDescriptor(DiagnosticId, Title, Message, Category, Severity, true);
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(Rule);
+
+        public override void Initialize(AnalysisContext context)
+        {
+            context.RegisterSyntaxNodeAction(AnalyzeSymbol, SyntaxKind.AsExpression);
+        }
+
+        private void AnalyzeSymbol(SyntaxNodeAnalysisContext context)
+        {
+            var literalExpression = context.Node as BinaryExpressionSyntax;
+            if (literalExpression == null)
+            {
+                return;
+            }
+
+            context.ReportDiagnostic(Diagnostic.Create(Rule, literalExpression.GetLocation()));
+        }
+    }
+}
