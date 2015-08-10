bd30dbf6667c193fca277d47a311f31ed981042b
Hosch250 <hosch250@live.com>
2015-08-10 15:58:46 -0500

    RemoveTestSuffix code fix.

--- VSDiagnostics/VSDiagnostics/VSDiagnostics.Test/Tests/Tests/RemoveTestSuffixTests.cs
+++ VSDiagnostics/VSDiagnostics/VSDiagnostics.Test/Tests/Tests/RemoveTestSuffixTests.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.CodeAnalysis.Diagnostics;
+﻿using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.Diagnostics;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using RoslynTester.Helpers.CSharp;
 using VSDiagnostics.Diagnostics.Tests.RemoveTestSuffix;
@@ -6,12 +7,14 @@ using VSDiagnostics.Diagnostics.Tests.RemoveTestSuffix;
 namespace VSDiagnostics.Test.Tests.Tests
 {
     [TestClass]
-    public class RemoveTestSuffixTests : CSharpDiagnosticVerifier
+    public class RemoveTestSuffixTests : CSharpCodeFixVerifier
     {
         protected override DiagnosticAnalyzer DiagnosticAnalyzer => new RemoveTestSuffixAnalyzer();
 
+        protected override CodeFixProvider CodeFixProvider => new RemoveTestSuffixCodeFix();
+
         [TestMethod]
-        public void RemoveTestSuffix_TestMethodEndsWithTest_InvokesWarning()
+        public void RemoveTestSuffix_TestMethodMethodEndsWithTest_InvokesWarning()
         {
             var original = @"
 using Microsoft.VisualStudio.TestTools.UnitTesting;
@@ -28,11 +31,27 @@ namespace ConsoleApplication1
     }
 }";
 
+            var result = @"
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ConsoleApplication1
+{
+    [TestClass]
+    class MyClass
+    {
+        [TestMethod]
+        public void Method()
+        {
+        }
+    }
+}";
+
             VerifyDiagnostic(original, string.Format(RemoveTestSuffixAnalyzer.Rule.MessageFormat.ToString(), "MethodTest"));
+            VerifyFix(original, result);
         }
 
         [TestMethod]
-        public void RemoveTestSuffix_TestMethodDoesNotEndWithTest_DoesNotInvokeWarning()
+        public void RemoveTestSuffix_TestMethodMethodDoesNotEndWithTest_DoesNotInvokeWarning()
         {
             var original = @"
 using Microsoft.VisualStudio.TestTools.UnitTesting;
@@ -77,6 +96,142 @@ namespace ConsoleApplication1
 {
     class MyClass
     {
+        public void Method()
+        {
+        }
+    }
+}";
+
+            VerifyDiagnostic(original);
+        }
+
+
+
+        [TestMethod]
+        public void RemoveTestSuffix_AttributedMethodEndsWithTest_DoesNotInvokeWarning()
+        {
+            var original = @"
+namespace ConsoleApplication1
+{
+    class MyClass
+    {
+        [Obsolete]
+        public void MethodTest()
+        {
+        }
+    }
+}";
+
+            VerifyDiagnostic(original);
+        }
+
+        [TestMethod]
+        public void RemoveTestSuffix_FactMethodEndsWithTest_InvokesWarning()
+        {
+            var original = @"
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ConsoleApplication1
+{
+    [TestClass]
+    class MyClass
+    {
+        [Fact]
+        public void MethodTest()
+        {
+        }
+    }
+}";
+
+            var result = @"
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ConsoleApplication1
+{
+    [TestClass]
+    class MyClass
+    {
+        [Fact]
+        public void Method()
+        {
+        }
+    }
+}";
+
+            VerifyDiagnostic(original, string.Format(RemoveTestSuffixAnalyzer.Rule.MessageFormat.ToString(), "MethodTest"));
+            VerifyFix(original, result);
+        }
+
+        [TestMethod]
+        public void RemoveTestSuffix_FactMethodDoesNotEndWithTest_DoesNotInvokeWarning()
+        {
+            var original = @"
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ConsoleApplication1
+{
+    [TestClass]
+    class MyClass
+    {
+        [Fact]
+        public void Method()
+        {
+        }
+    }
+}";
+
+            VerifyDiagnostic(original);
+        }
+
+        [TestMethod]
+        public void RemoveTestSuffix_TestMethodEndsWithTest_InvokesWarning()
+        {
+            var original = @"
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ConsoleApplication1
+{
+    [TestClass]
+    class MyClass
+    {
+        [Test]
+        public void MethodTest()
+        {
+        }
+    }
+}";
+
+            var result = @"
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ConsoleApplication1
+{
+    [TestClass]
+    class MyClass
+    {
+        [Test]
+        public void Method()
+        {
+        }
+    }
+}";
+
+            VerifyDiagnostic(original, string.Format(RemoveTestSuffixAnalyzer.Rule.MessageFormat.ToString(), "MethodTest"));
+            VerifyFix(original, result);
+        }
+
+        [TestMethod]
+        public void RemoveTestSuffix_TestMethodDoesNotEndWithTest_DoesNotInvokeWarning()
+        {
+            var original = @"
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ConsoleApplication1
+{
+    [TestClass]
+    class MyClass
+    {
+        [Test]
         public void Method()
         {
         }
---
+++ VSDiagnostics/VSDiagnostics/VSDiagnostics/Diagnostics/Tests/RemoveTestSuffix/RemoveTestSuffixCodeFix.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Immutable;
+using System.Composition;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeActions;
+using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Editing;
+
+namespace VSDiagnostics.Diagnostics.Tests.RemoveTestSuffix
+{
+    [ExportCodeFixProvider("RemoveTestSuffix", LanguageNames.CSharp), Shared]
+    public class RemoveTestSuffixCodeFix : CodeFixProvider
+    {
+        public override ImmutableArray<string> FixableDiagnosticIds => ImmutableArray.Create(RemoveTestSuffixAnalyzer.Rule.Id);
+
+        public override FixAllProvider GetFixAllProvider() => WellKnownFixAllProviders.BatchFixer;
+
+        public override async Task RegisterCodeFixesAsync(CodeFixContext context)
+        {
+            var root = await context.Document.GetSyntaxRootAsync(context.CancellationToken).ConfigureAwait(false);
+
+            var diagnostic = context.Diagnostics.First();
+            var diagnosticSpan = diagnostic.Location.SourceSpan;
+
+            var methodDeclaration = root.FindToken(diagnosticSpan.Start).Parent.AncestorsAndSelf().OfType<MethodDeclarationSyntax>().First();
+
+            context.RegisterCodeFix(CodeAction.Create("Make public", x => MakePublicAsync(context.Document, root, methodDeclaration), nameof(RemoveTestSuffixAnalyzer)), diagnostic);
+        }
+
+        private Task<Solution> MakePublicAsync(Document document, SyntaxNode root, MethodDeclarationSyntax method)
+        {
+            var generator = SyntaxGenerator.GetGenerator(document);
+            var newMethod = generator.WithName(method, method.Identifier.Text.Remove(method.Identifier.Text.Length - 4));
+            var newRoot = root.ReplaceNode(method, newMethod);
+            return Task.FromResult(document.WithSyntaxRoot(newRoot).Project.Solution);
+        }
+
+    }
+}
