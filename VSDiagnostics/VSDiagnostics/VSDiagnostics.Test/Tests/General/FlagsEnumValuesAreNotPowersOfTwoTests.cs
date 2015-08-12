180fc462d4efc2fce2c8486a19a96f75197b55cc
Hosch250 <hosch250@live.com>
2015-08-12 15:42:38 -0500

    Analyzer and analyzer tests for #143

---
+++ VSDiagnostics/VSDiagnostics/VSDiagnostics.Test/Tests/General/FlagsEnumValuesAreNotPowersOfTwoTests.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RoslynTester.Helpers.CSharp;
+using VSDiagnostics.Diagnostics.General.FlagsEnumValuesAreNotPowersOfTwo;
+
+namespace VSDiagnostics.Test.Tests.General
+{
+    [TestClass]
+    public class FlagsEnumValuesAreNotPowersOfTwoTests : CSharpDiagnosticVerifier
+    {
+        protected override DiagnosticAnalyzer DiagnosticAnalyzer => new FlagsEnumValuesAreNotPowersOfTwoAnalyzer();
+
+        [TestMethod]
+        public void FlagsEnumValuesAreNotPowersOfTwo_ValuesAreNotPowersOfTwo_InvokesWarning()
+        {
+            var original = @"
+namespace ConsoleApplication1
+{
+    [Flags]
+    enum Foo
+    {
+        Bar = 0,
+        Biz = 1,
+        Baz = 2,
+        Buz = 3,
+        Boz = 4,
+    }
+}";
+
+            VerifyDiagnostic(original, string.Format(FlagsEnumValuesAreNotPowersOfTwoAnalyzer.Rule.MessageFormat.ToString(), "Foo"));
+        }
+
+        [TestMethod]
+        public void FlagsEnumValuesAreNotPowersOfTwo_ValuesArePowersOfTwo_DoesNotInvokeWarning()
+        {
+            var original = @"
+namespace ConsoleApplication1
+{
+    [Flags]
+    enum Foo
+    {
+        Bar = 0,
+        Biz = 1,
+        Baz = 2,
+        Buz = 4,
+        Boz = 8,
+    }
+}";
+
+            VerifyDiagnostic(original);
+        }
+    }
+}
---
+++ VSDiagnostics/VSDiagnostics/VSDiagnostics/Diagnostics/General/FlagsEnumValuesAreNotPowersOfTwo/FlagsEnumValuesAreNotPowersOfTwoAnalyzer.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Immutable;
+using System.Linq;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace VSDiagnostics.Diagnostics.General.FlagsEnumValuesAreNotPowersOfTwo
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class FlagsEnumValuesAreNotPowersOfTwoAnalyzer : DiagnosticAnalyzer
+    {
+        private const string DiagnosticId = nameof(FlagsEnumValuesAreNotPowersOfTwoAnalyzer);
+        private const DiagnosticSeverity Severity = DiagnosticSeverity.Error;
+
+        private static readonly string Category = VSDiagnosticsResources.GeneralCategory;
+        private static readonly string Message = VSDiagnosticsResources.FlagsEnumValuesAreNotPowersOfTwoAnalyzerMessage;
+        private static readonly string Title = VSDiagnosticsResources.FlagsEnumValuesAreNotPowersOfTwoAnalyzerTitle;
+
+        internal static DiagnosticDescriptor Rule => new DiagnosticDescriptor(DiagnosticId, Title, Message, Category, Severity, true);
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(Rule);
+
+        public override void Initialize(AnalysisContext context)
+        {
+            context.RegisterSyntaxNodeAction(AnalyzeSymbol, SyntaxKind.EnumDeclaration);
+        }
+
+        private void AnalyzeSymbol(SyntaxNodeAnalysisContext context)
+        {
+            var declarationExpression = context.Node as EnumDeclarationSyntax;
+            if (declarationExpression == null)
+            {
+                return;
+            }
+
+            var enunMemberDeclarations = declarationExpression.ChildNodes().OfType<EnumMemberDeclarationSyntax>();
+
+            if (enunMemberDeclarations.Select(member => member.EqualsValue.Value as LiteralExpressionSyntax).Any(valueToken => !IsPowerOfTwo(int.Parse(valueToken.Token.ValueText))))
+            {
+                var enumName = context.SemanticModel.GetDeclaredSymbol(declarationExpression).Name;
+                context.ReportDiagnostic(Diagnostic.Create(Rule, declarationExpression.GetLocation(), enumName));
+            }
+        }
+
+        private bool IsPowerOfTwo(int value)
+        {
+            var valueAsBinary = Convert.ToString(value, 2);
+            return valueAsBinary == "0" || valueAsBinary.LastIndexOf('1') == 0;
+        }
+    }
+}
