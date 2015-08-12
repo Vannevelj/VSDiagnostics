5fd1eb3e30530b3e4e88f25bd99452c487b98733
Hosch250 <hosch250@live.com>
2015-08-11 19:14:46 -0500

    UseAliasesInsteadOfConcreteType analyzer and code fix.  Reference issue #22.

---
+++ VSDiagnostics/VSDiagnostics/VSDiagnostics/Diagnostics/General/UseAliasesInsteadOfConcreteType/UseAliasesInsteadOfConcreteTypeAnalyzer.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace VSDiagnostics.Diagnostics.General.UseAliasesInsteadOfConcreteType
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class UseAliasesInsteadOfConcreteTypeAnalyzer : DiagnosticAnalyzer
+    {
+        private const string DiagnosticId = nameof(UseAliasesInsteadOfConcreteTypeAnalyzer);
+        private const DiagnosticSeverity Severity = DiagnosticSeverity.Warning;
+
+        private static readonly string Category = VSDiagnosticsResources.GeneralCategory;
+        private static readonly string Message = VSDiagnosticsResources.UseAliasesInsteadOfConcreteTypeAnalyzerMessage;
+        private static readonly string Title = VSDiagnosticsResources.UseAliasesInsteadOfConcreteTypeAnalyzerTitle;
+
+        internal static DiagnosticDescriptor Rule => new DiagnosticDescriptor(DiagnosticId, Title, Message, Category, Severity, true);
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(Rule);
+
+        public override void Initialize(AnalysisContext context)
+        {
+            context.RegisterSyntaxNodeAction(AnalyzeSymbol, SyntaxKind.VariableDeclaration);
+        }
+
+        private void AnalyzeSymbol(SyntaxNodeAnalysisContext context)
+        {
+            var expression = context.Node as VariableDeclarationSyntax;
+
+            if (!(expression?.Type is IdentifierNameSyntax))
+            {
+                return;
+            }
+
+            var typeExpression = (IdentifierNameSyntax) expression.Type;
+
+            if (typeExpression.IsVar)
+            {
+                return;
+            }
+
+            var symbol = context.SemanticModel.GetSymbolInfo(typeExpression).Symbol;
+            var typeName = symbol.MetadataName;
+
+            if (symbol.ContainingNamespace.Name != "System")
+            {
+                return;
+            }
+
+            if (MapConcreteTypeToPredefinedTypeAlias.ContainsKey(typeName))
+            {
+                context.ReportDiagnostic(Diagnostic.Create(Rule, typeExpression.GetLocation(),
+                    MapConcreteTypeToPredefinedTypeAlias.First(kvp => kvp.Key == typeName).Value, typeName));
+            }
+        }
+
+        public static readonly Dictionary<string, string> MapConcreteTypeToPredefinedTypeAlias =
+            new Dictionary<string, string>
+            {
+                {"Int16", "short"},
+                {"Int32", "int"},
+                {"Int64", "long"},
+                {"UInt16", "ushort"},
+                {"UInt32", "uint"},
+                {"UInt64", "ulong"},
+                {"Object", "object"},
+                {"Byte", "byte"},
+                {"SByte", "sbyte"},
+                {"Char", "char"},
+                {"Boolean", "bool"},
+                {"Single", "float"},
+                {"Double", "double"},
+                {"Decimal", "decimal"},
+                {"String", "string"}
+            };
+    }
+}
---
+++ VSDiagnostics/VSDiagnostics/VSDiagnostics/Diagnostics/General/UseAliasesInsteadOfConcreteType/UseAliasesInsteadOfConcreteTypeCodeFix.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Immutable;
+using System.Composition;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeActions;
+using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Formatting;
+
+namespace VSDiagnostics.Diagnostics.General.UseAliasesInsteadOfConcreteType
+{
+    [ExportCodeFixProvider("UseAliasesInsteadOfConcreteType", LanguageNames.CSharp), Shared]
+    public class UseAliasesInsteadOfConcreteTypeCodeFix : CodeFixProvider
+    {
+        public override ImmutableArray<string> FixableDiagnosticIds => ImmutableArray.Create(UseAliasesInsteadOfConcreteTypeAnalyzer.Rule.Id);
+
+        public override FixAllProvider GetFixAllProvider() => WellKnownFixAllProviders.BatchFixer;
+
+        public override async Task RegisterCodeFixesAsync(CodeFixContext context)
+        {
+            var root = await context.Document.GetSyntaxRootAsync(context.CancellationToken).ConfigureAwait(false);
+            var diagnostic = context.Diagnostics.First();
+            var diagnosticSpan = diagnostic.Location.SourceSpan;
+
+            var statement = root.FindNode(diagnosticSpan);
+            context.RegisterCodeFix(CodeAction.Create(VSDiagnosticsResources.UseAliasesInsteadOfConcreteTypeCodeFixTitle, x => AsToCastAsync(context.Document, root, statement), nameof(UseAliasesInsteadOfConcreteTypeAnalyzer)), diagnostic);
+        }
+
+        private async Task<Solution> AsToCastAsync(Document document, SyntaxNode root, SyntaxNode statement)
+        {
+            var semanticModel = await document.GetSemanticModelAsync();
+
+            var typeExpression = (IdentifierNameSyntax) statement;
+
+            var typeName = semanticModel.GetSymbolInfo(typeExpression).Symbol.MetadataName;
+            var newName =
+                UseAliasesInsteadOfConcreteTypeAnalyzer.MapConcreteTypeToPredefinedTypeAlias.First(
+                    kvp => kvp.Key == typeName).Value;
+
+            var newExpression = SyntaxFactory.IdentifierName(newName);
+            var newRoot = root.ReplaceNode(typeExpression, newExpression).WithAdditionalAnnotations(Formatter.Annotation);
+            var newDocument = document.WithSyntaxRoot(newRoot);
+
+            return newDocument.Project.Solution;
+        }
+    }
+}
