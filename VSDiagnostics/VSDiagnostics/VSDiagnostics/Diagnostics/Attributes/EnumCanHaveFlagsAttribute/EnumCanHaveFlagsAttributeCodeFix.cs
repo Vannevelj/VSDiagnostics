5f4c34ccd3c508a16b49df825eaec2b8d3bd22c3
Hosch250 <hosch250@live.com>
2015-08-18 11:26:53 -0500

    Remove unnecessary "async" operator.

--- VSDiagnostics/VSDiagnostics/VSDiagnostics/Diagnostics/Attributes/EnumCanHaveFlagsAttribute/EnumCanHaveFlagsAttributeCodeFix.cs
+++ VSDiagnostics/VSDiagnostics/VSDiagnostics/Diagnostics/Attributes/EnumCanHaveFlagsAttribute/EnumCanHaveFlagsAttributeCodeFix.cs
@@ -28,7 +28,7 @@ namespace VSDiagnostics.Diagnostics.Attributes.EnumCanHaveFlagsAttribute
             context.RegisterCodeFix(CodeAction.Create(VSDiagnosticsResources.EnumCanHaveFlagsAttributeCodeFixTitle, x => AddFlagAttribute(context.Document, root, statement), nameof(EnumCanHaveFlagsAttributeAnalyzer)), diagnostic);
         }
 
-        private async Task<Solution> AddFlagAttribute(Document document, SyntaxNode root, SyntaxNode statement)
+        private Task<Solution> AddFlagAttribute(Document document, SyntaxNode root, SyntaxNode statement)
         {
             var enumDeclarationExpression = (EnumDeclarationSyntax) statement;
 
@@ -56,7 +56,7 @@ namespace VSDiagnostics.Diagnostics.Attributes.EnumCanHaveFlagsAttribute
             }
 
             var newDocument = document.WithSyntaxRoot(newRoot);
-            return newDocument.Project.Solution;
+            return Task.FromResult(newDocument.Project.Solution);
         }
     }
 }
