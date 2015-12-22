47cef75accf744b03818b3343a18a8d7801d7a26
Jeroen <jer_vannevel@hotmail.com>
2015-12-22 16:37:52 +0100

    1.9.0-beta metadata

--- VSDiagnostics/VSDiagnostics/VSDiagnostics/Properties/AssemblyInfo.cs
+++ VSDiagnostics/VSDiagnostics/VSDiagnostics/Properties/AssemblyInfo.cs
@@ -33,5 +33,5 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyVersion("1.9.0")]
 [assembly: AssemblyFileVersion("1.9.0.0")]
-[assembly: AssemblyInformationalVersion("1.9")]
+[assembly: AssemblyInformationalVersion("1.9 Beta")]
 [assembly: InternalsVisibleTo("VSDiagnostics.Test")]
