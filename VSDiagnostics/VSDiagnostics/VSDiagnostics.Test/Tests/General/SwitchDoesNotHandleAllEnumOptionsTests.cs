c8f076f8b5cae6f073817ca445e6039f2e657ebd
hosch250 <hosch250@live.com>
2016-03-13 20:20:37 -0500

    Initial implementation of SwitchDoesNotHandleAllEnumOptions analyzer and tests.

---
+++ VSDiagnostics/VSDiagnostics/VSDiagnostics.Test/Tests/General/SwitchDoesNotHandleAllEnumOptionsTests.cs
@@ -0,0 +1,156 @@
+﻿using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RoslynTester.Helpers.CSharp;
+using VSDiagnostics.Diagnostics.General.SwitchDoesNotHandleAllEnumOptions;
+
+namespace VSDiagnostics.Test.Tests.General
+{
+    [TestClass]
+    public class SwitchDoesNotHandleAllEnumOptionsTests : CSharpDiagnosticVerifier
+    {
+        protected override DiagnosticAnalyzer DiagnosticAnalyzer => new SwitchDoesNotHandleAllEnumOptionsAnalyzer();
+
+        [TestMethod]
+        public void SwitchDoesNotHandleAllEnumOptions_MissingEnumStatement()
+        {
+            var original = @"
+namespace ConsoleApplication1
+{
+    enum MyEnum
+    {
+        Fizz, Buzz, FizzBuzz
+    }
+
+    class MyClass
+    {
+        void Method()
+        {
+            var e = MyEnum.Fizz;
+            switch (e)
+            {
+                case MyEnum.Fizz:
+                case MyEnum.Buzz:
+                    break;
+            }
+        }
+    }
+}";
+
+            VerifyDiagnostic(original, SwitchDoesNotHandleAllEnumOptionsAnalyzer.Rule.MessageFormat.ToString());
+        }
+
+        [TestMethod]
+        public void SwitchDoesNotHandleAllEnumOptions_AllEnumStatements()
+        {
+            var original = @"
+namespace ConsoleApplication1
+{
+    enum MyEnum
+    {
+        Fizz, Buzz, FizzBuzz
+    }
+
+    class MyClass
+    {
+        void Method()
+        {
+            var e = MyEnum.Fizz;
+            switch (e)
+            {
+                case MyEnum.Fizz:
+                case MyEnum.Buzz:
+                case MyEnum.FizzBuzz:
+                    break;
+            }
+        }
+    }
+}";
+
+            VerifyDiagnostic(original);
+        }
+
+        [TestMethod]
+        public void SwitchDoesNotHandleAllEnumOptions_CaseStatementsNotEnum()
+        {
+            var original = @"
+namespace ConsoleApplication1
+{
+    class MyClass
+    {
+        void Method()
+        {
+            switch (""test"")
+            {
+                case ""Fizz"":
+                case ""Buzz"":
+                case ""FizzBuzz"":
+                    break;
+            }
+        }
+    }
+}";
+
+            VerifyDiagnostic(original);
+        }
+
+        [TestMethod]
+        public void SwitchDoesNotHandleAllEnumOptions_CaseHasDefaultStatement_MissingEnumStatement()
+        {
+            var original = @"
+namespace ConsoleApplication1
+{
+    enum MyEnum
+    {
+        Fizz, Buzz, FizzBuzz
+    }
+
+    class MyClass
+    {
+        void Method()
+        {
+            var e = MyEnum.Fizz;
+            switch (e)
+            {
+                case MyEnum.Fizz:
+                case MyEnum.Buzz:
+                default:
+                    break;
+            }
+        }
+    }
+}";
+
+            VerifyDiagnostic(original, SwitchDoesNotHandleAllEnumOptionsAnalyzer.Rule.MessageFormat.ToString());
+        }
+
+        [TestMethod]
+        public void SwitchDoesNotHandleAllEnumOptions_MissingEnumStatement_MultipleSections()
+        {
+            var original = @"
+namespace ConsoleApplication1
+{
+    enum MyEnum
+    {
+        Fizz, Buzz, FizzBuzz
+    }
+
+    class MyClass
+    {
+        void Method()
+        {
+            var e = MyEnum.Fizz;
+            switch (e)
+            {
+                case MyEnum.Fizz:
+                    break;
+                case MyEnum.Buzz:
+                    break;
+            }
+        }
+    }
+}";
+
+            VerifyDiagnostic(original, SwitchDoesNotHandleAllEnumOptionsAnalyzer.Rule.MessageFormat.ToString());
+        }
+    }
+}
---
+++ VSDiagnostics/VSDiagnostics/VSDiagnostics/Diagnostics/General/SwitchDoesNotHandleAllEnumOptions/SwitchDoesNotHandleAllEnumOptionsAnalyzer.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Immutable;
+using System.Linq;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+using VSDiagnostics.Utilities;
+
+namespace VSDiagnostics.Diagnostics.General.SwitchDoesNotHandleAllEnumOptions
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    internal class SwitchDoesNotHandleAllEnumOptionsAnalyzer : DiagnosticAnalyzer
+    {
+        private const DiagnosticSeverity Severity = DiagnosticSeverity.Warning;
+
+        private static readonly string Category = VSDiagnosticsResources.GeneralCategory;
+        private static readonly string Message = VSDiagnosticsResources.SingleEmptyConstructorAnalyzerMessage;
+        private static readonly string Title = VSDiagnosticsResources.SingleEmptyConstructorAnalyzerTitle;
+
+        internal static DiagnosticDescriptor Rule
+            => new DiagnosticDescriptor(DiagnosticId.SingleEmptyConstructor, Title, Message, Category, Severity, true);
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(Rule);
+
+        public override void Initialize(AnalysisContext context)
+        {
+            context.RegisterSyntaxNodeAction(AnalyzeSymbol, SyntaxKind.SwitchStatement);
+        }
+
+        private void AnalyzeSymbol(SyntaxNodeAnalysisContext context)
+        {
+            var switchBlock = context.Node as SwitchStatementSyntax;
+            if (switchBlock == null) { return; }
+
+            var enumType = context.SemanticModel.GetTypeInfo(switchBlock.Expression).Type as INamedTypeSymbol;
+            var v = nameof(System);
+            var t = nameof(System.Enum);
+            if (enumType == null || enumType.BaseType.ContainingNamespace.Name != nameof(System) || enumType.BaseType.Name != nameof(System.Enum)) { return; }
+
+            var labelNames = switchBlock.Sections.SelectMany(l => l.Labels)
+                    .OfType<CaseSwitchLabelSyntax>()
+                    .Select(l => l.Value)
+                    .OfType<MemberAccessExpressionSyntax>()
+                    .Select(l => l.Name.Identifier.ValueText)
+                    .ToList();
+
+            if (enumType.MemberNames.Any(member => !labelNames.Contains(member)))
+            {
+                context.ReportDiagnostic(Diagnostic.Create(Rule, switchBlock.GetLocation()));
+            }
+        }
+    }
+}
