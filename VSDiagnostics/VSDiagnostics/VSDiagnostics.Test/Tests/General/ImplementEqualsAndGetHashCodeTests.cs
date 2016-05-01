287919277648a54d859cfe7f821e1e2dd8f20a25
Abraham Hosch <hosch250@live.com>
2016-05-01 14:39:59 -0500

    More tests.

--- VSDiagnostics/VSDiagnostics/VSDiagnostics.Test/Tests/General/ImplementEqualsAndGetHashCodeTests.cs
+++ VSDiagnostics/VSDiagnostics/VSDiagnostics.Test/Tests/General/ImplementEqualsAndGetHashCodeTests.cs
@@ -623,5 +623,281 @@ namespace ConsoleApplication1
             VerifyDiagnostic(original, string.Format(ImplementEqualsAndGetHashCodeAnalyzer.Rule.MessageFormat.ToString(), "MyClass"));
             VerifyFix(original, result);
         }
+
+        [TestMethod]
+        public void ImplementEqualsAndGetHashCode_ClassDoesNotImplementEither_HasBaseClassImplementingEquals_HasInterface()
+        {
+            var original = @"
+namespace ConsoleApplication1
+{
+    interface IClass { }
+
+    class MyBaseClass
+    {
+        public override bool Equals(object obj) => true;
+    }
+
+    class MyClass : MyBaseClass, IClass
+    {
+        string _foo = ""test"";
+        static string _bar = ""test"";
+    }
+}";
+
+            var result = @"
+namespace ConsoleApplication1
+{
+    interface IClass { }
+
+    class MyBaseClass
+    {
+        public override bool Equals(object obj) => true;
+    }
+
+    class MyClass : MyBaseClass, IClass
+    {
+        string _foo = ""test"";
+        static string _bar = ""test"";
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null || typeof(MyClass) != obj.GetType())
+            {
+                return false;
+            }
+
+            var value = (MyClass)obj;
+            return base.Equals(obj) &&
+                   _foo == value._foo;
+        }
+
+        public override int GetHashCode()
+        {
+            return _foo.GetHashCode();
+        }
+    }
+}";
+
+            VerifyDiagnostic(original, string.Format(ImplementEqualsAndGetHashCodeAnalyzer.Rule.MessageFormat.ToString(), "MyClass"));
+            VerifyFix(original, result);
+        }
+
+        [TestMethod]
+        public void ImplementEqualsAndGetHashCode_StructDoesNotImplementEither_ImplementsInterface()
+        {
+            var original = @"
+namespace ConsoleApplication1
+{
+    interface IStruct { }
+
+    struct MyStruct : IStruct
+    {
+        string _foo;
+        static string _bar;
+    }
+}";
+
+            var result = @"
+namespace ConsoleApplication1
+{
+    interface IStruct { }
+
+    struct MyStruct : IStruct
+    {
+        string _foo;
+        static string _bar;
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null || typeof(MyStruct) != obj.GetType())
+            {
+                return false;
+            }
+
+            var value = (MyStruct)obj;
+            return _foo == value._foo;
+        }
+
+        public override int GetHashCode()
+        {
+            return _foo.GetHashCode();
+        }
+    }
+}";
+
+            VerifyDiagnostic(original, string.Format(ImplementEqualsAndGetHashCodeAnalyzer.Rule.MessageFormat.ToString(), "MyStruct"));
+            VerifyFix(original, result);
+        }
+
+        [TestMethod]
+        public void ImplementEqualsAndGetHashCode_ClassDoesNotImplementEither_HasBaseClassImplementingEquals_BaseClassHasField()
+        {
+            var original = @"
+namespace ConsoleApplication1
+{
+    class MyBaseClass
+    {
+        public string foo;
+        public override bool Equals(object obj) => true;
+    }
+
+    class MyClass : MyBaseClass
+    {
+        string _foo = ""test"";
+        static string _bar = ""test"";
+    }
+}";
+
+            var result = @"
+namespace ConsoleApplication1
+{
+    class MyBaseClass
+    {
+        public string foo;
+        public override bool Equals(object obj) => true;
+    }
+
+    class MyClass : MyBaseClass
+    {
+        string _foo = ""test"";
+        static string _bar = ""test"";
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null || typeof(MyClass) != obj.GetType())
+            {
+                return false;
+            }
+
+            var value = (MyClass)obj;
+            return base.Equals(obj) &&
+                   _foo == value._foo;
+        }
+
+        public override int GetHashCode()
+        {
+            return _foo.GetHashCode();
+        }
+    }
+}";
+
+            VerifyDiagnostic(original, string.Format(ImplementEqualsAndGetHashCodeAnalyzer.Rule.MessageFormat.ToString(), "MyClass"));
+            VerifyFix(original, result);
+        }
+
+        [TestMethod]
+        public void ImplementEqualsAndGetHashCode_ClassDoesNotImplementEither_HasBaseClassNotImplementingEquals()
+        {
+            var original = @"
+namespace ConsoleApplication1
+{
+    class MyBaseClass
+    {
+        public string foo;
+        public override int GetHashCode() => 1;  // disable analyzer for this
+    }
+
+    class MyClass : MyBaseClass
+    {
+        string _foo = ""test"";
+        static string _bar = ""test"";
+    }
+}";
+
+            var result = @"
+namespace ConsoleApplication1
+{
+    class MyBaseClass
+    {
+        public string foo;
+        public override int GetHashCode() => 1;  // disable analyzer for this
+    }
+
+    class MyClass : MyBaseClass
+    {
+        string _foo = ""test"";
+        static string _bar = ""test"";
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null || typeof(MyClass) != obj.GetType())
+            {
+                return false;
+            }
+
+            var value = (MyClass)obj;
+            return _foo == value._foo;
+        }
+
+        public override int GetHashCode()
+        {
+            return _foo.GetHashCode();
+        }
+    }
+}";
+
+            VerifyDiagnostic(original, string.Format(ImplementEqualsAndGetHashCodeAnalyzer.Rule.MessageFormat.ToString(), "MyClass"));
+            VerifyFix(original, result);
+        }
+
+        [TestMethod]
+        public void ImplementEqualsAndGetHashCode_ClassDoesImplementsEquals_OverridesPropertyInBaseClass()
+        {
+            var original = @"
+namespace ConsoleApplication1
+{
+    class MyBaseClass
+    {
+        public virtual string Bar { get; set; }
+        public override bool Equals(object obj) => true;
+    }
+
+    class MyClass : MyBaseClass
+    {
+        string _foo = ""test"";
+        static string _bar = ""test"";
+        public override string Bar { get; set; }
+    }
+}";
+
+            var result = @"
+namespace ConsoleApplication1
+{
+    class MyBaseClass
+    {
+        public virtual string Bar { get; set; }
+        public override bool Equals(object obj) => true;
+    }
+
+    class MyClass : MyBaseClass
+    {
+        string _foo = ""test"";
+        static string _bar = ""test"";
+        public override string Bar { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null || typeof(MyClass) != obj.GetType())
+            {
+                return false;
+            }
+
+            var value = (MyClass)obj;
+            return base.Equals(obj) &&
+                   _foo == value._foo &&
+                   Bar == value.Bar;
+        }
+
+        public override int GetHashCode()
+        {
+            return _foo.GetHashCode() ^
+                   Bar.GetHashCode();
+        }
+    }
+}";
+
+            VerifyDiagnostic(original, string.Format(ImplementEqualsAndGetHashCodeAnalyzer.Rule.MessageFormat.ToString(), "MyClass"));
+            VerifyFix(original, result);
+        }
     }
 }
